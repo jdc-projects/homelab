c4a2f1fb3f5fefef69065b4653b0bc4d6cc29cc3
Jack Chapman <Jack.Chapman@Credera.co.uk>
2023-03-26 17:30:21 +0100

    Another small fix.

--- terraform/cluster-config-write.sh
+++ terraform/cluster-config-write.sh
@@ -4,4 +4,3 @@ sed -i'' -e "s#{server-domain}#$TF_VARS_server_base_domain#g" ./cluster.yml
 sed -i'' -e "s#{server-port}#$SERVER_KUBE_PORT#g" ./cluster.yml
 sed -i'' -e "s#{client-certificate-data}#$KUBE_CLIENT_CERT_DATA#g" ./cluster.yml
 sed -i'' -e "s#{client-key-data}#$KUBE_CLIENT_KEY_DATA#g" ./cluster.yml
-rm cluster.yml-e
