b8527289f64ddd493ba4a3e7114f636dbd176860
Jack Chapman <Jack.Chapman@Credera.co.uk>
2023-03-26 17:28:48 +0100

    Fixes to kubeconfig generation script.

--- terraform/cluster-config-write.sh
+++ terraform/cluster-config-write.sh
@@ -1,6 +1,7 @@
 cp cluster.yml.template cluster.yml
-sed -i "s#{certificate-authority-data}#$KUBE_CLUSTER_CA_CERT_DATA#g" ./cluster.yml
-sed -i "s#{server-domain}#$SERVER_BASE_DOMAIN#g" ./cluster.yml
-sed -i "s#{server-port}#$SERVER_KUBE_PORT#g" ./cluster.yml
-sed -i "s#{client-certificate-data}#$KUBE_CLIENT_CERT_DATA#g" ./cluster.yml
-sed -i "s#{client-key-data}#$KUBE_CLIENT_KEY_DATA#g" ./cluster.yml
+sed -i'' -e "s#{certificate-authority-data}#$KUBE_CLUSTER_CA_CERT_DATA#g" ./cluster.yml
+sed -i'' -e "s#{server-domain}#$TF_VARS_server_base_domain#g" ./cluster.yml
+sed -i'' -e "s#{server-port}#$SERVER_KUBE_PORT#g" ./cluster.yml
+sed -i'' -e "s#{client-certificate-data}#$KUBE_CLIENT_CERT_DATA#g" ./cluster.yml
+sed -i'' -e "s#{client-key-data}#$KUBE_CLIENT_KEY_DATA#g" ./cluster.yml
+rm cluster.yml-e
