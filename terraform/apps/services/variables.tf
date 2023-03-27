b2542d82369ce409494dedb4e042117dfb07432d
Jack Chapman <Jack.Chapman@Credera.co.uk>
2023-03-27 17:09:40 +0100

    Terraform format.

--- terraform/apps/services/variables.tf
+++ terraform/apps/services/variables.tf
@@ -5,18 +5,18 @@ variable "server_base_domain" {
 
 variable "cloudflare_ddns_token" {
   type        = string
-  sensitive = true
+  sensitive   = true
   description = "API key for the dynamic DNS for Cloudflare."
 }
 
 variable "idrac_username" {
   type        = string
-  sensitive = true
+  sensitive   = true
   description = "Username for server idrac."
 }
 
 variable "idrac_password" {
   type        = string
-  sensitive = true
+  sensitive   = true
   description = "Password for server idrac."
 }
