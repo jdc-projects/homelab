83cdd8f8876bd81fb90d5da57fe81090e4989bbf
Jack Chapman <Jack.Chapman@Credera.co.uk>
2023-03-29 23:42:13 +0100

    Terraform format...

--- terraform/apps/vaultwarden/deployment.tf
+++ terraform/apps/vaultwarden/deployment.tf
@@ -29,7 +29,7 @@ resource "truenas_dataset" "vaultwarden_dataset" {
   inherit_encryption = true
 
   lifecycle {
-    prevent_destroy = false
+    prevent_destroy       = false
     create_before_destroy = true
   }
 }
