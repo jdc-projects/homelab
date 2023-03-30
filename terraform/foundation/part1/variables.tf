375d72154f440d5a1f8bbfe03d3a2d410316f1e9
Jack Chapman <Jack.Chapman@Credera.co.uk>
2023-03-30 22:06:54 +0100

    Forgot to include variable change.

--- terraform/foundation/part1/variables.tf
+++ terraform/foundation/part1/variables.tf
@@ -1,3 +1,8 @@
+variable "server_base_domain" {
+  type        = string
+  description = "Domain of the Truenas Scale instance."
+}
+
 variable "jdc_projects_runners_app_id" {
   type        = string
   description = ""
