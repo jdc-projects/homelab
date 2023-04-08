ce106de184e599573938608f412c37792904cc4f
Jack Chapman <Jack.Chapman@Credera.co.uk>
2023-04-08 18:44:19 +0100

    Wrong value for keycloak outputs.

--- terraform/apps/keycloak/output.tf
+++ terraform/apps/keycloak/output.tf
@@ -1,10 +1,10 @@
 output "keycloak_admin_username" {
-  value     = random_password.keycloak_admin_username
+  value     = random_password.keycloak_admin_username.result
   sensitive = true
 }
 
 output "keycloak_admin_password" {
-  value     = random_password.keycloak_admin_password
+  value     = random_password.keycloak_admin_password.result
   sensitive = true
 }
 
