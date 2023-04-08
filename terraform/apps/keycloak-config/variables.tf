557d344be88ccf2509b66e918dce8123ece725ed
Jack Chapman <Jack.Chapman@Credera.co.uk>
2023-04-08 18:35:55 +0100

    Another missing variable.

--- terraform/apps/keycloak-config/variables.tf
+++ terraform/apps/keycloak-config/variables.tf
@@ -2,3 +2,9 @@ variable "server_base_domain" {
   type        = string
   description = "Domain of the Truenas Scale instance."
 }
+
+variable "lldap_admin_password" {
+  type        = string
+  sensitive   = true
+  description = "Password for the LLDAP admin user."
+}
