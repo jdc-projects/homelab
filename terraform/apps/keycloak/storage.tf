35b9bad82f456b5e6a80c458a2e3815e25fb170f
Jack Chapman <Jack.Chapman@Credera.co.uk>
2023-04-08 03:36:04 +0100

    Remove the other dataset, since it isn't used.

--- terraform/apps/keycloak/storage.tf
+++ terraform/apps/keycloak/storage.tf
@@ -1,31 +1,9 @@
-resource "truenas_dataset" "keycloak_base_dataset" {
-  pool               = "vault"
-  name               = "keycloak"
-  inherit_encryption = true
-
-  lifecycle {
-    prevent_destroy = false
-  }
-}
-
-resource "truenas_dataset" "keycloak_dataset" {
-  pool               = "vault"
-  parent             = truenas_dataset.keycloak_base_dataset.name
-  name               = "keycloak"
-  inherit_encryption = true
-
-  lifecycle {
-    prevent_destroy = false
-  }
-}
-
 resource "truenas_dataset" "keycloak_db_dataset" {
   pool               = "vault"
-  parent             = truenas_dataset.keycloak_base_dataset.name
-  name               = "db"
+  name               = "keycloak-db"
   inherit_encryption = true
 
   lifecycle {
-    prevent_destroy = false
+    prevent_destroy = true
   }
 }
