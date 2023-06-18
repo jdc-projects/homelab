6dbb3c175468a1b06917e9d0bbb069c58d3926fb
Jack Chapman <jack.chapman@sky.com>
2023-06-18 18:16:30 +0100

    Manually imported the dataset.

--- terraform/apps/ldap/storage.tf
+++ terraform/apps/ldap/storage.tf
@@ -1,8 +1,3 @@
-import {
-  to = truenas_dataset.lldap_dataset
-  id = "vault/lldap"
-}
-
 resource "truenas_dataset" "lldap_dataset" {
   pool               = "vault"
   name               = "lldap"
