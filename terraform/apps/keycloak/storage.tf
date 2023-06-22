aa3bbbdeef4682d32c47de45d9229a0e0808953a
Jack Chapman <jack.chapman@sky.com>
2023-06-22 16:31:44 +0100

    Remove apps that I'm not using.

--- terraform/apps/keycloak/storage.tf
+++
@@ -1,9 +0,0 @@
-resource "truenas_dataset" "keycloak_db_dataset" {
-  pool               = "vault/apps"
-  name               = "keycloak-db"
-  inherit_encryption = true
-
-  lifecycle {
-    prevent_destroy = true
-  }
-}
--- terraform/apps/nextcloud/storage.tf
+++
@@ -1,9 +0,0 @@
-resource "truenas_dataset" "nextcloud_dataset" {
-  pool               = "vault"
-  name               = "nextcloud"
-  inherit_encryption = true
-
-  lifecycle {
-    prevent_destroy = false
-  }
-}
