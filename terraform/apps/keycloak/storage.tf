b7b6dc4d8f9c071b4592667027c4865d289934f9
Jack Chapman <jack.chapman@sky.com>
2023-06-24 19:21:37 +0100

    Fix dataset for Keycloak.

--- terraform/apps/keycloak/storage.tf
+++ terraform/apps/keycloak/storage.tf
@@ -1,5 +1,6 @@
 resource "truenas_dataset" "keycloak_db_dataset" {
-  pool               = "vault/apps"
+  pool               = "vault"
+  parent             = "apps"
   name               = "keycloak-db"
   inherit_encryption = true
 
