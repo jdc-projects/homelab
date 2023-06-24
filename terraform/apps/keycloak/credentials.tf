30f1d43c4964e120ef3815156fced44f753dbfce
Jack Chapman <jack.chapman@sky.com>
2023-06-24 22:17:46 +0100

    Remove upper case characters from password.

--- terraform/apps/keycloak/credentials.tf
+++ terraform/apps/keycloak/credentials.tf
@@ -2,22 +2,26 @@ resource "random_password" "keycloak_admin_username" {
   length  = 16
   numeric = false
   special = false
+  upper   = false
 }
 
 resource "random_password" "keycloak_admin_password" {
   length  = 16
   numeric = true
   special = false
+  upper   = true
 }
 
 resource "random_password" "db_admin_username" {
   length  = 16
   numeric = false
   special = false
+  upper   = false
 }
 
 resource "random_password" "db_admin_password" {
   length  = 16
   numeric = true
   special = false
+  upper   = true
 }
