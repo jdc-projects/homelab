25f443bc71d5a18490b3fcc188ee574a8d30b3ea
Jack Chapman <jack.chapman@sky.com>
2023-06-25 16:55:19 +0100

    Better output name.

--- terraform/apps/keycloak-config/outputs.tf
+++ terraform/apps/keycloak-config/outputs.tf
@@ -12,6 +12,6 @@ output "keycloak_hostname_url" {
   value = data.terraform_remote_state.keycloak.outputs.keycloak_hostname_url
 }
 
-output "keycloak_lldap_domain_name" {
+output "keycloak_lldap_realm_id" {
   value = keycloak_realm.lldap_realm.realm
 }
