5e169d64335ada45f34e6b49d91daf37d0e54499
Jack Chapman <jack.chapman@sky.com>
2023-06-25 15:31:31 +0100

    Add outputs to Keycloak config for consilidation.

---
+++ terraform/apps/keycloak-config/outputs.tf
@@ -0,0 +1,17 @@
+output "keycloak_admin_username" {
+  value     = data.terraform_remote_state.keycloak.outputs.keycloak_admin_username
+  sensitive = true
+}
+
+output "keycloak_admin_password" {
+  value     = data.terraform_remote_state.keycloak.outputs.keycloak_admin_password
+  sensitive = true
+}
+
+output "keycloak_hostname_url" {
+  value = data.terraform_remote_state.keycloak.outputs.keycloak_hostname_url
+}
+
+output "keycloak_lldap_domain_name" {
+  value = keycloak_realm.lldap_realm.realm
+}
