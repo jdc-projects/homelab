03f1825eb2b2e9be824d83a88f4134f0d0d2a4f3
Jack Chapman <jack.chapman@sky.com>
2023-06-25 20:00:34 +0100

    Add a sleep after the repo clone, since it fails sometimes.

--- terraform/apps/ocis/ocis-helm-repo.tf
+++ terraform/apps/ocis/ocis-helm-repo.tf
@@ -1,5 +1,5 @@
 resource "null_resource" "ocis_helm_repo" {
   provisioner "local-exec" {
-    command = "git clone --depth 1 -b v0.4.0 https://github.com/owncloud/ocis-charts.git"
+    command = "git clone --depth 1 -b v0.4.0 https://github.com/owncloud/ocis-charts.git && sleep 1"
   }
 }
