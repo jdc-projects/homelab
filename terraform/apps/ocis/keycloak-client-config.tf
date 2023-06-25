ad8bd2948966cc5f09e1182b735c8e6a06c8e9eb
Jack Chapman <jack.chapman@sky.com>
2023-06-25 19:45:31 +0100

    Terraform format.

--- terraform/apps/ocis/keycloak-client-config.tf
+++ terraform/apps/ocis/keycloak-client-config.tf
@@ -1,18 +1,18 @@
 resource "keycloak_openid_client" "openid_client" {
-  realm_id            = data.terraform_remote_state.keycloak_config.outputs.keycloak_lldap_realm_id
-  client_id           = "ocis-web"
+  realm_id  = data.terraform_remote_state.keycloak_config.outputs.keycloak_lldap_realm_id
+  client_id = "ocis-web"
 
-  name                = "ocis-web"
-  enabled             = true
+  name    = "ocis-web"
+  enabled = true
 
-  access_type         = "PUBLIC"
+  access_type = "PUBLIC"
   valid_redirect_uris = [
     "https://ocis.${var.server_base_domain}/*"
   ]
 
   client_authenticator_type = "client-jwt"
 
-  standard_flow_enabled = true
+  standard_flow_enabled        = true
   direct_access_grants_enabled = true
 
   full_scope_allowed = true
