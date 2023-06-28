0992e6787055d0a3cebee4a9babde41b9a4c1e92
Jack Chapman <jack.chapman@sky.com>
2023-06-28 13:47:56 +0100

    Terraform fmt.

--- terraform/apps/keycloak-config/keycloak-config.tf
+++ terraform/apps/keycloak-config/keycloak-config.tf
@@ -29,7 +29,7 @@ resource "keycloak_ldap_user_federation" "lldap_user_federation" {
 
   pagination = false
 
-  full_sync_period    = 600
+  full_sync_period = 600
 }
 
 resource "keycloak_ldap_group_mapper" "lldap_group_mapper" {
