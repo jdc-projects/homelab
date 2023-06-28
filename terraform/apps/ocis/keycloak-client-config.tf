5412661a165b90ef3062a611eb26a4dcf5b58665
Jack Chapman <jack.chapman@sky.com>
2023-06-28 13:05:42 +0100

    Trying a different openid client setup.

--- terraform/apps/ocis/keycloak-client-config.tf
+++ terraform/apps/ocis/keycloak-client-config.tf
@@ -5,17 +5,17 @@ resource "keycloak_openid_client" "ocis_web_client" {
   name    = "ocis-web"
   enabled = true
 
-  access_type = "PUBLIC"
+  access_type = "CONFIDENTIAL"
   valid_redirect_uris = [
     "https://ocis.${var.server_base_domain}/*"
   ]
 
   client_authenticator_type = "client-jwt"
 
-#   standard_flow_enabled        = true
-#   direct_access_grants_enabled = true
+  standard_flow_enabled        = true
+  direct_access_grants_enabled = true
 
-#   full_scope_allowed = true
+  full_scope_allowed = true
 
   login_theme = "keycloak"
 }
