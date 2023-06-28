1277f052d884b771f76029ca5e52eb6e9cb1bc36
Jack Chapman <jack.chapman@sky.com>
2023-06-28 20:05:18 +0100

    Trying some escape characters.

--- terraform/apps/ocis/ocis-deployment.tf
+++ terraform/apps/ocis/ocis-deployment.tf
@@ -117,7 +117,7 @@ resource "helm_release" "ocis" {
   }
   set {
     name  = "features.externalUserManagement.ldap.user.baseDN"
-    value = "ou=users,dc=owncloud,dc=com"
+    value = "ou=users\\,dc=owncloud\\,dc=com"
   }
   set {
     name  = "features.externalUserManagement.ldap.user.schema.filter"
