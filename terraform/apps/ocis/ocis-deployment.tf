628a10bfc985bc5acd7dedf582e2aa063185a923
Jack Chapman <jack.chapman@sky.com>
2023-06-28 20:48:02 +0100

    Testing if OCIS is actually connected to the LDAP server.

--- terraform/apps/ocis/ocis-deployment.tf
+++ terraform/apps/ocis/ocis-deployment.tf
@@ -5,7 +5,7 @@ resource "kubernetes_secret" "ocis_ldap_password_secret" {
   }
 
   data = {
-    reva-ldap-bind-password = var.lldap_admin_password
+    reva-ldap-bind-password = "rubbishValue"
   }
 }
 
