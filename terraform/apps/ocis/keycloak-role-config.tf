5bd17c11b23d4524e43691c867076f49639e7ab6
Jack Chapman <jack.chapman@sky.com>
2023-06-30 13:59:38 +0100

    This should create the role mappings.

--- terraform/apps/ocis/keycloak-role-config.tf
+++ terraform/apps/ocis/keycloak-role-config.tf
@@ -1,16 +1,63 @@
-# resource "keycloak_ldap_role_mapper" "ocis_admin_role_mapper" {
-#   realm_id                = keycloak_realm.realm.id
-#   ldap_user_federation_id = keycloak_ldap_user_federation.ldap_user_federation.id
-#   name                    = "role-mapper"
+resource "keycloak_role" "ocis_admin_client_role" {
+  realm_id    = data.terraform_remote_state.keycloak_config.outputs.keycloak_lldap_realm_id
+  client_id   = null_resource.ocis_keycloak_client_id.triggers.client_id
+  name        = "ocisAdmin"
+  description = "OCIS Admin"
+}
 
-#   ldap_roles_dn                 = "dc=example,dc=org"
-#   role_name_ldap_attribute      = "cn"
-#   role_object_classes           = [
-#     "groupOfNames"
-#   ]
-#   membership_attribute_type      = "DN"
-#   membership_ldap_attribute      = "member"
-#   membership_user_ldap_attribute = "cn"
-#   user_roles_retrieve_strategy   = "GET_ROLES_FROM_USER_MEMBEROF_ATTRIBUTE"
-#   memberof_ldap_attribute        = "memberOf"
-# }
+resource "keycloak_role" "ocis_space_admin_client_role" {
+  realm_id    = data.terraform_remote_state.keycloak_config.outputs.keycloak_lldap_realm_id
+  client_id   = null_resource.ocis_keycloak_client_id.triggers.client_id
+  name        = "ocisSpaceAdmin"
+  description = "OCIS Space Admin"
+}
+
+resource "keycloak_role" "ocis_user_client_role" {
+  realm_id    = data.terraform_remote_state.keycloak_config.outputs.keycloak_lldap_realm_id
+  client_id   = null_resource.ocis_keycloak_client_id.triggers.client_id
+  name        = "ocisUser"
+  description = "OCIS User"
+}
+
+resource "keycloak_role" "ocis_guest_client_role" {
+  realm_id    = data.terraform_remote_state.keycloak_config.outputs.keycloak_lldap_realm_id
+  client_id   = null_resource.ocis_keycloak_client_id.triggers.client_id
+  name        = "ocisGuest"
+  description = "OCIS Guest"
+}
+
+resource "keycloak_group_roles" "ocis_admin_group_role" {
+  realm_id = data.terraform_remote_state.keycloak_config.outputs.keycloak_lldap_realm_id
+  group_id = "ocis_admin"
+
+  role_ids = [
+    keycloak_role.ocis_admin_client_role.id
+  ]
+}
+
+resource "keycloak_group_roles" "ocis_space_admin_group_role" {
+  realm_id = data.terraform_remote_state.keycloak_config.outputs.keycloak_lldap_realm_id
+  group_id = "ocis_space_admin"
+
+  role_ids = [
+    keycloak_role.ocis_space_admin_client_role.id
+  ]
+}
+
+resource "keycloak_group_roles" "ocis_user_group_role" {
+  realm_id = data.terraform_remote_state.keycloak_config.outputs.keycloak_lldap_realm_id
+  group_id = "ocis_user"
+
+  role_ids = [
+    keycloak_role.ocis_user_client_role.id
+  ]
+}
+
+resource "keycloak_group_roles" "ocis_guest_group_role" {
+  realm_id = data.terraform_remote_state.keycloak_config.outputs.keycloak_lldap_realm_id
+  group_id = "ocis_guest"
+
+  role_ids = [
+    keycloak_role.ocis_guest_client_role.id
+  ]
+}
