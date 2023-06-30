90088d37fe5349ae0c3b9f5abb678ea341ba3f43
Jack Chapman <jack.chapman@sky.com>
2023-06-30 17:06:09 +0100

    Failed to save first...

--- terraform/apps/ocis/keycloak-client-config.tf
+++ terraform/apps/ocis/keycloak-client-config.tf
@@ -45,15 +45,16 @@ resource "keycloak_openid_client_default_scopes" "ocis_client_default_scopes" {
 }
 
 resource "keycloak_openid_user_client_role_protocol_mapper" "ocis_client_role_claim_mapper" {
-  realm_id  = data.terraform_remote_state.keycloak_config.outputs.keycloak_jack_chapman_co_uk_realm_id
-  client_id = keycloak_openid_client.ocis_client.id
-
-  name                        = "role-mapper"
-  claim_name                  = "roles"
-  claim_value_type            = "String"
-  multivalued                 = true
+  realm_id                    = data.terraform_remote_state.keycloak_config.outputs.keycloak_jack_chapman_co_uk_realm_id
+  client_id                   = keycloak_openid_client.ocis_client.id
   client_id_for_role_mappings = keycloak_openid_client.ocis_client.id
-  add_to_id_token             = false
-  add_to_access_token         = false
-  add_to_userinfo             = true
+
+  name             = "role-mapper"
+  claim_name       = "roles"
+  claim_value_type = "String"
+  multivalued      = true
+
+  add_to_id_token     = false
+  add_to_access_token = false
+  add_to_userinfo     = true
 }
