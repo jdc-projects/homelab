b490761d5b32792b8da03bed4646554ce00ebdee
Jack Chapman <jack.chapman@sky.com>
2023-07-01 15:32:54 +0100

    Revert "Widen the scope on the OCIS roles."
    This reverts commit a108a7994dfd45c08482e6c341910e6a738fa6cf.

--- terraform/apps/ocis/keycloak-roles.tf
+++ terraform/apps/ocis/keycloak-roles.tf
@@ -1,23 +1,27 @@
 resource "keycloak_role" "ocis_admin_client_role" {
   realm_id    = data.terraform_remote_state.keycloak_config.outputs.keycloak_jack_chapman_co_uk_realm_id
+  client_id   = keycloak_openid_client.ocis_web_client.id
   name        = "ocisAdmin"
   description = "OCIS Admin"
 }
 
 resource "keycloak_role" "ocis_space_admin_client_role" {
   realm_id    = data.terraform_remote_state.keycloak_config.outputs.keycloak_jack_chapman_co_uk_realm_id
+  client_id   = keycloak_openid_client.ocis_web_client.id
   name        = "ocisSpaceAdmin"
   description = "OCIS Space Admin"
 }
 
 resource "keycloak_role" "ocis_user_client_role" {
   realm_id    = data.terraform_remote_state.keycloak_config.outputs.keycloak_jack_chapman_co_uk_realm_id
+  client_id   = keycloak_openid_client.ocis_web_client.id
   name        = "ocisUser"
   description = "OCIS User"
 }
 
 resource "keycloak_role" "ocis_guest_client_role" {
   realm_id    = data.terraform_remote_state.keycloak_config.outputs.keycloak_jack_chapman_co_uk_realm_id
+  client_id   = keycloak_openid_client.ocis_web_client.id
   name        = "ocisGuest"
   description = "OCIS Guest"
 }
