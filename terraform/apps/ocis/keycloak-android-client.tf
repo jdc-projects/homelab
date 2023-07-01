91adade87b247e2aca78b0c5c52f3f7daabe8cdb
Jack Chapman <jack.chapman@sky.com>
2023-07-01 14:53:00 +0100

    Add default and optional scope definitions back in.

--- terraform/apps/ocis/keycloak-android-client.tf
+++ terraform/apps/ocis/keycloak-android-client.tf
@@ -23,6 +23,33 @@ resource "keycloak_openid_client" "ocis_android_client" {
   login_theme = "keycloak"
 }
 
+resource "keycloak_openid_client_optional_scopes" "ocis_android_client_optional_scopes" {
+  realm_id  = data.terraform_remote_state.keycloak_config.outputs.keycloak_jack_chapman_co_uk_realm_id
+  client_id = keycloak_openid_client.ocis_android_client.id
+
+  optional_scopes = [
+    "address",
+    "phone",
+    "offline_access",
+    "microprofile-jwt"
+  ]
+}
+
+resource "keycloak_openid_client_default_scopes" "ocis_android_client_default_scopes" {
+  realm_id  = data.terraform_remote_state.keycloak_config.outputs.keycloak_jack_chapman_co_uk_realm_id
+  client_id = keycloak_openid_client.ocis_android_client.id
+
+  default_scopes = [
+    "web-origins",
+    "role_list",
+    "profile",
+    "roles",
+    "email"
+  ]
+
+  depends_on = [keycloak_openid_client_optional_scopes.ocis_android_client_optional_scopes]
+}
+
 resource "keycloak_openid_user_client_role_protocol_mapper" "ocis_android_client_role_claim_mapper" {
   realm_id  = data.terraform_remote_state.keycloak_config.outputs.keycloak_jack_chapman_co_uk_realm_id
   client_id = keycloak_openid_client.ocis_android_client.id
--- terraform/apps/ocis/keycloak-desktop-client.tf
+++ terraform/apps/ocis/keycloak-desktop-client.tf
@@ -24,6 +24,33 @@ resource "keycloak_openid_client" "ocis_desktop_client" {
   login_theme = "keycloak"
 }
 
+resource "keycloak_openid_client_optional_scopes" "ocis_desktop_client_optional_scopes" {
+  realm_id  = data.terraform_remote_state.keycloak_config.outputs.keycloak_jack_chapman_co_uk_realm_id
+  client_id = keycloak_openid_client.ocis_desktop_client.id
+
+  optional_scopes = [
+    "address",
+    "phone",
+    "offline_access",
+    "microprofile-jwt"
+  ]
+}
+
+resource "keycloak_openid_client_default_scopes" "ocis_desktop_client_default_scopes" {
+  realm_id  = data.terraform_remote_state.keycloak_config.outputs.keycloak_jack_chapman_co_uk_realm_id
+  client_id = keycloak_openid_client.ocis_desktop_client.id
+
+  default_scopes = [
+    "web-origins",
+    "role_list",
+    "profile",
+    "roles",
+    "email"
+  ]
+
+  depends_on = [keycloak_openid_client_optional_scopes.ocis_desktop_client_optional_scopes]
+}
+
 resource "keycloak_openid_user_client_role_protocol_mapper" "ocis_desktop_client_role_claim_mapper" {
   realm_id  = data.terraform_remote_state.keycloak_config.outputs.keycloak_jack_chapman_co_uk_realm_id
   client_id = keycloak_openid_client.ocis_desktop_client.id
