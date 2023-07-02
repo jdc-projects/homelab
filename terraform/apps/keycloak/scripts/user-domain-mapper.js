6f25d985d6f680001c16ad3245dbd254f0545041
Jack Chapman <jack.chapman@sky.com>
2023-07-02 23:55:06 +0100

    Ok, simplifying the script down, we should be able to adapt this.

--- terraform/apps/keycloak/scripts/user-domain-mapper.js
+++ terraform/apps/keycloak/scripts/user-domain-mapper.js
@@ -1,19 +1,2 @@
-// you can set standard fields in token
-token.setAcr("test value");
-
 // you can set claims in the token
-token.getOtherClaims().put("claimName", "claim value");
-
-// multi-valued claim (thanks to @ErwinRooijakkers)
-token.getOtherClaims().put('foo', Java.to(['bars'], "java.lang.String[]"))
-
-// work with variables and return multivalued token value
-var ArrayList = Java.type("java.util.ArrayList");
-var roles = new ArrayList();
-var client = keycloakSession.getContext().getClient();
-var forEach = Array.prototype.forEach;
-forEach.call(user.getClientRoleMappings(client).toArray(), function(roleModel) {
-  roles.add(roleModel.getName());
-});
-
-exports = roles;
+token.getOtherClaims().put("userDomainId", user.getUsername);
