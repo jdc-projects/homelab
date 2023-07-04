acf84f8cd8c2d6f7e54ad05c474d0a90debe6d18
Jack Chapman <jack.chapman@sky.com>
2023-07-04 06:08:00 +0100

    Revert "Force recreation of Mariadb for debug."
    This reverts commit 30d4cbb696e8f93ccd3775220dfe8399e9c84d68.

--- terraform/apps/seafile/mariadb.tf
+++ terraform/apps/seafile/mariadb.tf
@@ -30,10 +30,4 @@ resource "helm_release" "mariadb" {
     name  = "volumePermissions.enabled"
     value = "true"
   }
-
-  lifecycle {
-    replace_triggered_by = [
-      null_resource.seafile_config_file_population # DEBUG
-    ]
-  }
 }
