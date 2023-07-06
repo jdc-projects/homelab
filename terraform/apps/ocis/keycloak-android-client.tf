a6400051e44201172db00d5198d6046d63d66c17
Jack Chapman <jack.chapman@sky.com>
2023-07-07 00:05:36 +0100

    Add Android and IOS Keycloak clients for OCIS.

---
+++ terraform/apps/ocis/keycloak-android-client.tf
@@ -0,0 +1,22 @@
+resource "keycloak_openid_client" "ocis_desktop_client" {
+  realm_id  = data.terraform_remote_state.keycloak_config.outputs.keycloak_jack_chapman_co_uk_realm_id
+  client_id = "e4rAsNUSIUs0lF4nbv9FmCeUkTlV9GdgTLDH1b5uie7syb90SzEVrbN7HIpmWJeD"
+
+  name    = "ocis-android"
+  enabled = true
+
+  access_type = "PUBLIC"
+  valid_redirect_uris = [
+    "oc://android.owncloud.com"
+  ]
+
+  client_authenticator_type = "client-jwt"
+
+  standard_flow_enabled        = true
+  direct_access_grants_enabled = true
+  implicit_flow_enabled        = true
+
+  full_scope_allowed = true
+
+  login_theme = "keycloak"
+}
---
+++ terraform/apps/ocis/keycloak-ios-client.tf
@@ -0,0 +1,22 @@
+resource "keycloak_openid_client" "ocis_desktop_client" {
+  realm_id  = data.terraform_remote_state.keycloak_config.outputs.keycloak_jack_chapman_co_uk_realm_id
+  client_id = "mxd5OQDk6es5LzOzRvidJNfXLUZS2oN3oUFeXPP8LpPrhx3UroJFduGEYIBOxkY1"
+
+  name    = "ocis-ios"
+  enabled = true
+
+  access_type = "PUBLIC"
+  valid_redirect_uris = [
+    "oc://ios.owncloud.com"
+  ]
+
+  client_authenticator_type = "client-jwt"
+
+  standard_flow_enabled        = true
+  direct_access_grants_enabled = true
+  implicit_flow_enabled        = true
+
+  full_scope_allowed = true
+
+  login_theme = "keycloak"
+}
