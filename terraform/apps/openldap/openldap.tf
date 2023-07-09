8627e91ba9289dceb94b863285a7ebf04d25a471
Jack Chapman <jack.chapman@sky.com>
2023-07-09 18:45:50 +0100

    Fix for dependency, and following best practice.

--- terraform/apps/openldap/openldap.tf
+++ terraform/apps/openldap/openldap.tf
@@ -1,24 +1,3 @@
-resource "null_resource" "get_custom_ldif" {
-  provisioner "local-exec" {
-    command = <<-EOF
-        mkdir ./ldifs
-        cd ./ldifs
-        wget https://raw.githubusercontent.com/osixia/docker-openldap/635034a75878773f8576d646422cf26e43741fab/image/service/slapd/assets/config/bootstrap/schema/rfc2307bis.ldif
-      EOF
-  }
-}
-
-resource "kubernetes_config_map" "openldap_custom_ldifs" {
-  metadata {
-    name      = "openldap-custom-ldifs"
-    namespace = kubernetes_namespace.openldap.metadata[0].name
-  }
-
-  data = {
-    "rfc2307bis.ldif" = file("./ldifs/rfc2307bis.ldif")
-  }
-}
-
 resource "kubernetes_config_map" "openldap_env" {
   metadata {
     name      = "openldap-env"
