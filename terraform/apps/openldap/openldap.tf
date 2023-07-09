d96ad7873c8e0e3b8236fcbd60bc481c02cf8a9c
Jack Chapman <jack.chapman@sky.com>
2023-07-09 20:43:08 +0100

    It might need a config admin.

--- terraform/apps/openldap/openldap.tf
+++ terraform/apps/openldap/openldap.tf
@@ -5,12 +5,15 @@ resource "kubernetes_config_map" "openldap_env" {
   }
 
   data = {
-    LDAP_PORT_NUMBER       = "1389"
-    LDAP_ROOT              = "dc=idm,dc=${var.server_base_domain}"
-    LDAP_ADMIN_USERNAME    = random_password.openldap_admin_username.result
-    LDAP_SKIP_DEFAULT_TREE = "yes"
-    LDAP_ADD_SCHEMAS       = "no"
-    BITNAMI_DEBUG          = "true"
+    LDAP_PORT_NUMBER           = "1389"
+    LDAP_ROOT                  = "dc=idm,dc=${var.server_base_domain}"
+    LDAP_ADMIN_USERNAME        = random_password.openldap_admin_username.result
+    LDAP_CONFIG_ADMIN_ENABLED  = "yes"
+    LDAP_CONFIG_ADMIN_USERNAME = random_password.openldap_config_admin_username.result
+    LDAP_CONFIG_ADMIN_PASSWORD = random_password.openldap_config_admin_password.result
+    LDAP_SKIP_DEFAULT_TREE     = "yes"
+    LDAP_ADD_SCHEMAS           = "no"
+    BITNAMI_DEBUG              = "true"
   }
 }
 
