687223e0203df7079e9e468af5da3ce3e2602990
Jack Chapman <jack.chapman@sky.com>
2023-07-09 16:58:17 +0100

    Remove extra schemas to see what it does.

--- terraform/apps/openldap/openldap.tf
+++ terraform/apps/openldap/openldap.tf
@@ -9,6 +9,7 @@ resource "kubernetes_config_map" "openldap_env" {
     LDAP_ROOT              = "dc=idm,dc=${var.server_base_domain}"
     LDAP_ADMIN_USERNAME    = random_password.openldap_admin_username.result
     LDAP_SKIP_DEFAULT_TREE = "yes"
+    LDAP_ADD_SCHEMAS       = "no"
   }
 }
 
