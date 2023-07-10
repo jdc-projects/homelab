60dd0376e5e52f2afd3a76d3a507243efb0b11d4
Jack Chapman <jack.chapman@sky.com>
2023-07-10 20:33:00 +0100

    Network setup for LUM.

--- terraform/apps/openldap/network.tf
+++ terraform/apps/openldap/network.tf
@@ -34,6 +34,24 @@ resource "kubernetes_service" "phpldapadmin" {
   }
 }
 
+resource "kubernetes_service" "ldap_user_manager" {
+  metadata {
+    name      = "ldap-user-manager"
+    namespace = kubernetes_namespace.openldap.metadata[0].name
+  }
+
+  spec {
+    selector = {
+      app = "ldap-user-manager"
+    }
+
+    port {
+      port        = "80"
+      target_port = kubernetes_config_map.ldap_user_manager_env.data.LDAP_PORT_NUMBER
+    }
+  }
+}
+
 resource "kubernetes_manifest" "openldap_ingress" {
   manifest = {
     apiVersion = "traefik.containo.us/v1alpha1"
@@ -82,6 +100,32 @@ resource "kubernetes_manifest" "phpldapadmin_ingress" {
           name      = kubernetes_service.phpldapadmin.metadata[0].name
           namespace = kubernetes_namespace.openldap.metadata[0].name
           port      = kubernetes_service.phpldapadmin.spec[0].port[0].port
+        }]
+      }]
+    }
+  }
+}
+
+resource "kubernetes_manifest" "ldap_user_manager_ingress" {
+  manifest = {
+    apiVersion = "traefik.containo.us/v1alpha1"
+    kind       = "IngressRoute"
+
+    metadata = {
+      name      = "ldap-user-manager"
+      namespace = kubernetes_namespace.openldap.metadata[0].name
+    }
+
+    spec = {
+      entryPoints = ["websecure"]
+
+      routes = [{
+        kind  = "Rule"
+        match = "Host(`idm3.${var.server_base_domain}`)"
+        services = [{
+          name      = kubernetes_service.ldap_user_manager.metadata[0].name
+          namespace = kubernetes_namespace.openldap.metadata[0].name
+          port      = kubernetes_service.ldap_user_manager.spec[0].port[0].port
         }]
       }]
     }
