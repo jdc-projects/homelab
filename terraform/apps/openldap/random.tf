e490aa79ca4ab1d7b220c0c44e332bcd2c0635c8
Jack Chapman <jack.chapman@sky.com>
2023-07-10 20:58:18 +0100

    Add some test users for the new groups.

--- terraform/apps/openldap/random.tf
+++ terraform/apps/openldap/random.tf
@@ -11,3 +11,45 @@ resource "random_password" "openldap_admin_password" {
   special = false
   upper   = true
 }
+
+resource "random_password" "openldap_test_admin_username" {
+  length  = 16
+  numeric = false
+  special = false
+  upper   = false
+}
+
+resource "random_password" "openldap_test_admin_password" {
+  length  = 16
+  numeric = true
+  special = false
+  upper   = true
+}
+
+resource "random_password" "openldap_test_user_username" {
+  length  = 16
+  numeric = false
+  special = false
+  upper   = false
+}
+
+resource "random_password" "openldap_test_user_password" {
+  length  = 16
+  numeric = true
+  special = false
+  upper   = true
+}
+
+resource "random_password" "openldap_test_guest_username" {
+  length  = 16
+  numeric = false
+  special = false
+  upper   = false
+}
+
+resource "random_password" "openldap_test_guest_password" {
+  length  = 16
+  numeric = true
+  special = false
+  upper   = true
+}
