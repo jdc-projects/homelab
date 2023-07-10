e5f42258b45057eab61a3910126911f393746872
Jack Chapman <jack.chapman@sky.com>
2023-07-10 20:16:25 +0100

    Add SMTP variables for LUM.

--- terraform/apps/openldap/variables.tf
+++ terraform/apps/openldap/variables.tf
@@ -8,3 +8,24 @@ variable "truenas_api_key" {
   sensitive   = true
   description = "API key for the Truenas server instance."
 }
+
+variable "smtp_host" {
+  type        = string
+  description = "Hostname of the SMTP server."
+}
+
+variable "smtp_port" {
+  type        = string
+  description = "Port of the SMTP server."
+}
+
+variable "smtp_username" {
+  type        = string
+  description = "Username for the SMTP server."
+}
+
+variable "smtp_password" {
+  type        = string
+  sensitive   = true
+  description = "Password for the SMTP server."
+}
