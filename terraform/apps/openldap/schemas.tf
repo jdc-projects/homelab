799ec50698b2a315042fde438f46ad904c819b96
Jack Chapman <jack.chapman@sky.com>
2023-07-10 01:04:42 +0100

    Don't need to pull the file anymore.

--- terraform/apps/openldap/schemas.tf
+++ terraform/apps/openldap/schemas.tf
@@ -1,17 +1,3 @@
-resource "null_resource" "get_custom_schemas" {
-  triggers = {
-    always_run = timestamp()
-  }
-
-  # provisioner "local-exec" {
-  #   command = <<-EOF
-  #       mkdir ./schemas
-  #       cd ./schemas
-  #       curl https://raw.githubusercontent.com/github/github-ldap/master/test/fixtures/openldap/memberof.ldif -o memberof.ldif
-  #     EOF
-  # }
-}
-
 data "local_file" "memberof" {
   filename = "./schemas/memberof.ldif"
 
