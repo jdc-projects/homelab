8c1a0b5a61c6873fb13ced5b1c6d65b4431a6a31
Jack Chapman <jack.chapman@sky.com>
2023-07-10 20:30:55 +0100

    Initial config for LUM.

---
+++ terraform/apps/openldap/ldap-user-manager.tf
@@ -0,0 +1,90 @@
+resource "kubernetes_config_map" "ldap_user_manager_env" {
+  metadata {
+    name      = "ldap-user-manager-env"
+    namespace = kubernetes_namespace.openldap.metadata[0].name
+  }
+
+  data = {
+    LDAP_URI              = "ldaps://idm.${var.server_base_domain}:637"
+    LDAP_BASE_DN          = "dc=idm,dc=${var.server_base_domain}"
+    LDAP_ADMIN_BIND_DN    = "cn=${random_password.openldap_admin_username.result},dc=idm,dc=${var.server_base_domain}"
+    LDAP_ADMINS_GROUP     = "system_admins"
+    SERVER_HOSTNAME       = "idm3.${var.server_base_domain}"
+    SERVER_PORT           = "80"
+    NO_HTTPS              = "TRUE"
+    ORGANISATION_NAME     = var.server_base_domain
+    SITE_NAME             = "${var.server_base_domain} Account Management"
+    LDAP_REQUIRE_STARTTLS = "FALSE"
+    DEFAULT_USER_GROUP    = "app_guests"
+    USERNAME_FORMAT       = "{first_name}"
+    SMTP_HOSTNAME         = var.smtp_host
+    SMTP_HOST_PORT        = var.smtp_port
+    SMTP_USERNAME         = var.smtp_username
+    SMTP_PASSWORD         = var.smtp_password
+    SMTP_USE_TLS          = "TRUE"
+    EMAIL_FROM_ADDRESS    = "noreply@${var.server_base_domain}"
+    EMAIL_FROM_NAME       = "LDAP User Manager ${var.server_base_domain}"
+  }
+}
+
+resource "kubernetes_secret" "ldap_user_manager_env" {
+  metadata {
+    name      = "ldap-user-manager-env"
+    namespace = kubernetes_namespace.openldap.metadata[0].name
+  }
+
+  data = {
+    LDAP_ADMIN_BIND_PWD = random_password.openldap_admin_password.result
+  }
+}
+
+resource "kubernetes_deployment" "ldap_user_manager" {
+  metadata {
+    name      = "ldap-user-manager"
+    namespace = kubernetes_namespace.openldap.metadata[0].name
+  }
+
+  spec {
+    replicas = 1
+
+    selector {
+      match_labels = {
+        app = "ldap-user-manager"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "ldap-user-manager"
+        }
+      }
+
+      spec {
+        container {
+          image = "wheelybird/ldap-user-manager:v1.10"
+          name  = "phpldapadmin"
+
+          env_from {
+            config_map_ref {
+              name = kubernetes_config_map.ldap_user_manager_env.metadata[0].name
+            }
+          }
+
+          env_from {
+            secret_ref {
+              name = kubernetes_secret.ldap_user_manager_env.metadata[0].name
+            }
+          }
+        }
+      }
+    }
+  }
+
+  lifecycle {
+    replace_triggered_by = [
+      kubernetes_config_map.ldap_user_manager_env,
+      kubernetes_secret.ldap_user_manager_env
+    ]
+  }
+}
