9a9451b94bc449d5a13c3b6ea6f0369209a62dd3
Jack Chapman <jack.chapman@sky.com>
2023-07-11 01:52:39 +0100

    OCIS helm repo cleanup errors out randomly, so remove it for now.

--- terraform/apps/ocis/ocis-helm-repo.tf
+++ terraform/apps/ocis/ocis-helm-repo.tf
@@ -7,15 +7,3 @@ resource "null_resource" "ocis_helm_repo_clone" {
     command = "git clone --depth 1 -b v0.4.0 https://github.com/owncloud/ocis-charts.git"
   }
 }
-
-resource "null_resource" "ocis_helm_repo_cleanup" {
-  triggers = {
-    always_run = timestamp()
-  }
-
-  provisioner "local-exec" {
-    command = "rm -rf ocis-charts"
-  }
-
-  depends_on = [helm_release.ocis]
-}
