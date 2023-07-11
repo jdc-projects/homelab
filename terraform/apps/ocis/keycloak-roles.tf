c12e90e179cc1b25e1ce8cea424e286f177208b4
Jack Chapman <jack.chapman@sky.com>
2023-07-11 08:27:07 +0100

    Helps if I actually include the for_each field.

--- terraform/apps/ocis/keycloak-roles.tf
+++ terraform/apps/ocis/keycloak-roles.tf
@@ -67,6 +67,8 @@ resource "keycloak_group_roles" "ocis_admin" {
 }
 
 resource "keycloak_group_roles" "ocis_user" {
+  for_each = keycloak_role.ocis_admin
+
   realm_id = data.terraform_remote_state.keycloak_config.outputs.keycloak_jack_chapman_co_uk_realm_id
   group_id = data.keycloak_group.app_users.id
 
@@ -76,6 +78,8 @@ resource "keycloak_group_roles" "ocis_user" {
 }
 
 resource "keycloak_group_roles" "ocis_guest" {
+  for_each = keycloak_role.ocis_admin
+
   realm_id = data.terraform_remote_state.keycloak_config.outputs.keycloak_jack_chapman_co_uk_realm_id
   group_id = data.keycloak_group.app_guests.id
 
