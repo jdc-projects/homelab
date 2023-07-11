b02acada7482e4815b50303569bcf7a63b6e4c0d
Jack Chapman <jack.chapman@sky.com>
2023-07-11 06:24:21 +0100

    Add role mapper for OCIS web client.

--- terraform/apps/ocis/keycloak-web-client.tf
+++ terraform/apps/ocis/keycloak-web-client.tf
@@ -25,3 +25,17 @@ resource "keycloak_openid_client" "ocis_web" {
 
   login_theme = "keycloak"
 }
+
+resource "keycloak_openid_user_client_role_protocol_mapper" "user_client_role_mapper" {
+  realm_id                    = data.terraform_remote_state.keycloak_config.outputs.keycloak_jack_chapman_co_uk_realm_id
+  client_id                   = keycloak_openid_client.ocis_web.id
+  name                        = "role-mapper"
+  claim_name                  = "roles"
+  client_id_for_role_mappings = keycloak_openid_client.ocis_web.id
+
+  multivalued = "true"
+
+  add_to_id_token     = "false"
+  add_to_access_token = "false"
+  add_to_userinfo     = "true"
+}
