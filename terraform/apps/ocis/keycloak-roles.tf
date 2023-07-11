546957837dc952b51a7fea8334f53184058a696f
Jack Chapman <jack.chapman@sky.com>
2023-07-11 09:36:31 +0100

    Revert "Try it a way that doesn't use the whole object."
    This reverts commit b5eb495d6aabf0b4870c7c4ac27b2a4c45781b6d.

--- terraform/apps/ocis/keycloak-roles.tf
+++ terraform/apps/ocis/keycloak-roles.tf
@@ -90,17 +90,17 @@ resource "keycloak_group_roles" "ocis_guest" {
 
 resource "keycloak_openid_user_client_role_protocol_mapper" "ocis_claim_mapper" {
   for_each = toset([
-    [keycloak_openid_client.ocis_web.id, keycloak_openid_client.ocis_web.client_id],
-    [keycloak_openid_client.ocis_desktop.id, keycloak_openid_client.ocis_desktop.client_id],
-    [keycloak_openid_client.ocis_android.id, keycloak_openid_client.ocis_android.client_id],
-    [keycloak_openid_client.ocis_ios.id, keycloak_openid_client.ocis_ios.client_id]
+    keycloak_openid_client.ocis_web,
+    keycloak_openid_client.ocis_desktop,
+    keycloak_openid_client.ocis_android,
+    keycloak_openid_client.ocis_ios
   ])
 
   realm_id                    = data.terraform_remote_state.keycloak_config.outputs.keycloak_jack_chapman_co_uk_realm_id
-  client_id                   = each.value[0]
+  client_id                   = each.value.id
   name                        = "role-mapper"
   claim_name                  = "roles"
-  client_id_for_role_mappings = each.value[1]
+  client_id_for_role_mappings = each.value.client_id
 
   multivalued = "true"
 
