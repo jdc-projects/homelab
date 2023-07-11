56b842b3fe6b1509269acdf129603b37cc02ff75
Jack Chapman <jack.chapman@sky.com>
2023-07-11 12:48:16 +0100

    Trying some dependencies for the group role mappings.

--- terraform/apps/ocis/keycloak-roles.tf
+++ terraform/apps/ocis/keycloak-roles.tf
@@ -64,6 +64,12 @@ resource "keycloak_group_roles" "ocis_admin" {
   role_ids = [
     each.value.id
   ]
+
+  depends_on = [
+    keycloak_role.ocis_admin,
+    keycloak_role.ocis_user,
+    keycloak_role.ocis_guest
+  ]
 }
 
 resource "keycloak_group_roles" "ocis_user" {
@@ -75,6 +81,12 @@ resource "keycloak_group_roles" "ocis_user" {
   role_ids = [
     each.value.id
   ]
+
+  depends_on = [
+    keycloak_role.ocis_admin,
+    keycloak_role.ocis_user,
+    keycloak_role.ocis_guest
+  ]
 }
 
 resource "keycloak_group_roles" "ocis_guest" {
@@ -85,6 +97,12 @@ resource "keycloak_group_roles" "ocis_guest" {
 
   role_ids = [
     each.value.id
+  ]
+
+  depends_on = [
+    keycloak_role.ocis_admin,
+    keycloak_role.ocis_user,
+    keycloak_role.ocis_guest
   ]
 }
 
