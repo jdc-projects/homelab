8d010ce793af05931791308724de469e821c1f07
Jack Chapman <jack.chapman@sky.com>
2023-07-11 14:33:44 +0100

    Manually create group-role mappings.

---
+++ terraform/apps/ocis/keycloak-ios-roles.tf
@@ -0,0 +1,26 @@
+resource "keycloak_group_roles" "ocis_admin_ios" {
+  realm_id = data.terraform_remote_state.keycloak_config.outputs.keycloak_jack_chapman_co_uk_realm_id
+  group_id = data.keycloak_group.app_admins.id
+
+  role_ids = [
+    keycloak_role.ocis_admin["ocis_ios"].id
+  ]
+}
+
+resource "keycloak_group_roles" "ocis_user_ios" {
+  realm_id = data.terraform_remote_state.keycloak_config.outputs.keycloak_jack_chapman_co_uk_realm_id
+  group_id = data.keycloak_group.app_users.id
+
+  role_ids = [
+    keycloak_role.ocis_user["ocis_ios"].id
+  ]
+}
+
+resource "keycloak_group_roles" "ocis_guest_ios" {
+  realm_id = data.terraform_remote_state.keycloak_config.outputs.keycloak_jack_chapman_co_uk_realm_id
+  group_id = data.keycloak_group.app_guests.id
+
+  role_ids = [
+    keycloak_role.ocis_guest["ocis_ios"].id
+  ]
+}
---
+++ terraform/apps/ocis/keycoak-web-roles.tf
@@ -0,0 +1,26 @@
+resource "keycloak_group_roles" "ocis_admin_web" {
+  realm_id = data.terraform_remote_state.keycloak_config.outputs.keycloak_jack_chapman_co_uk_realm_id
+  group_id = data.keycloak_group.app_admins.id
+
+  role_ids = [
+    keycloak_role.ocis_admin["ocis_web"].id
+  ]
+}
+
+resource "keycloak_group_roles" "ocis_user_web" {
+  realm_id = data.terraform_remote_state.keycloak_config.outputs.keycloak_jack_chapman_co_uk_realm_id
+  group_id = data.keycloak_group.app_users.id
+
+  role_ids = [
+    keycloak_role.ocis_user["ocis_web"].id
+  ]
+}
+
+resource "keycloak_group_roles" "ocis_guest_web" {
+  realm_id = data.terraform_remote_state.keycloak_config.outputs.keycloak_jack_chapman_co_uk_realm_id
+  group_id = data.keycloak_group.app_guests.id
+
+  role_ids = [
+    keycloak_role.ocis_guest["ocis_web"].id
+  ]
+}
