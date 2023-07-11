f2e1bb50c8c049b8b569df849828fa5849a50a4f
Jack Chapman <jack.chapman@sky.com>
2023-07-11 13:31:28 +0100

    More clever (but long and still dumb) wait period.

--- terraform/apps/ocis/keycloak-roles.tf
+++ terraform/apps/ocis/keycloak-roles.tf
@@ -15,55 +15,91 @@ data "keycloak_group" "app_guests" {
 
 resource "keycloak_role" "ocis_admin" {
   for_each = tomap({
-    ocis_web_id     = keycloak_openid_client.ocis_web.id
-    ocis_desktop_id = keycloak_openid_client.ocis_desktop.id
-    ocis_android_id = keycloak_openid_client.ocis_android.id
-    ocis_ios_id     = keycloak_openid_client.ocis_ios.id
+    ocis_web = tomap({
+      id        = keycloak_openid_client.ocis_web.id
+      wait = "1"
+    })
+    ocis_desktop = tomap({
+      id        = keycloak_openid_client.ocis_desktop.id
+      wait = "3"
+    })
+    ocis_android = tomap({
+      id        = keycloak_openid_client.ocis_android.id
+      wait = "5"
+    })
+    ocis_ios = tomap({
+      id        = keycloak_openid_client.ocis_ios.id
+      wait = "7"
+    })
   })
 
   realm_id    = data.terraform_remote_state.keycloak_config.outputs.keycloak_jack_chapman_co_uk_realm_id
-  client_id   = each.value
+  client_id   = each.value.id
   name        = "ocisAdmin"
   description = "OCIS Admin"
 
   provisioner "local-exec" {
-    command = "sleep 1"
+    command = "sleep ${each.value.wait}"
   }
 }
 
 resource "keycloak_role" "ocis_user" {
   for_each = tomap({
-    ocis_web_id     = keycloak_openid_client.ocis_web.id
-    ocis_desktop_id = keycloak_openid_client.ocis_desktop.id
-    ocis_android_id = keycloak_openid_client.ocis_android.id
-    ocis_ios_id     = keycloak_openid_client.ocis_ios.id
+    ocis_web = tomap({
+      id        = keycloak_openid_client.ocis_web.id
+      wait = "9"
+    })
+    ocis_desktop = tomap({
+      id        = keycloak_openid_client.ocis_desktop.id
+      wait = "11"
+    })
+    ocis_android = tomap({
+      id        = keycloak_openid_client.ocis_android.id
+      wait = "13"
+    })
+    ocis_ios = tomap({
+      id        = keycloak_openid_client.ocis_ios.id
+      wait = "15"
+    })
   })
 
   realm_id    = data.terraform_remote_state.keycloak_config.outputs.keycloak_jack_chapman_co_uk_realm_id
-  client_id   = each.value
+  client_id   = each.value.id
   name        = "ocisUser"
   description = "OCIS User"
 
   provisioner "local-exec" {
-    command = "sleep 1"
+    command = "sleep ${each.value.wait}"
   }
 }
 
 resource "keycloak_role" "ocis_guest" {
   for_each = tomap({
-    ocis_web_id     = keycloak_openid_client.ocis_web.id
-    ocis_desktop_id = keycloak_openid_client.ocis_desktop.id
-    ocis_android_id = keycloak_openid_client.ocis_android.id
-    ocis_ios_id     = keycloak_openid_client.ocis_ios.id
+    ocis_web = tomap({
+      id        = keycloak_openid_client.ocis_web.id
+      wait = "17"
+    })
+    ocis_desktop = tomap({
+      id        = keycloak_openid_client.ocis_desktop.id
+      wait = "19"
+    })
+    ocis_android = tomap({
+      id        = keycloak_openid_client.ocis_android.id
+      wait = "21"
+    })
+    ocis_ios = tomap({
+      id        = keycloak_openid_client.ocis_ios.id
+      wait = "23"
+    })
   })
 
   realm_id    = data.terraform_remote_state.keycloak_config.outputs.keycloak_jack_chapman_co_uk_realm_id
-  client_id   = each.value
+  client_id   = each.value.id
   name        = "ocisGuest"
   description = "OCIS Guest"
 
   provisioner "local-exec" {
-    command = "sleep 1"
+    command = "sleep ${each.value.wait}"
   }
 }
 
