a6c74c713388f81d1e708e14cf86fb95e0d80fdc
Jack Chapman <jack.chapman@sky.com>
2023-07-12 22:07:42 +0100

    Wrong realm.

--- terraform/apps/keycloak-config/role_mapping.tf
+++ terraform/apps/keycloak-config/role_mapping.tf
@@ -1,15 +1,15 @@
 data "keycloak_group" "system_admins" {
-  realm_id = keycloak_realm.server_base_domain.id
+  realm_id = data.keycloak_realm.master.id
   name     = "system_admins"
 }
 
 data "keycloak_role" "admin" {
-  realm_id = keycloak_realm.server_base_domain.id
+  realm_id = data.keycloak_realm.master.id
   name     = "admin"
 }
 
 resource "keycloak_group_roles" "system_admins_admin" {
-  realm_id = keycloak_realm.server_base_domain.id
+  realm_id = data.keycloak_realm.master.id
   group_id = data.keycloak_group.system_admins.id
 
   role_ids = [
