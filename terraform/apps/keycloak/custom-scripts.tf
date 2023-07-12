4013ae302266783267e8abdbbc814b192f3c4552
Jack Chapman <jack.chapman@sky.com>
2023-07-12 20:50:12 +0100

    Reorganisation for future-proofing.

---
+++ terraform/apps/keycloak/custom-scripts.tf
@@ -0,0 +1,19 @@
+data "archive_file" "keycloak_scripts_jar" {
+  type             = "zip"
+  output_path      = "./scripts.jar"
+  source_dir       = "./scripts"
+  output_file_mode = "0444"
+}
+
+resource "kubernetes_config_map" "keycloak_custom_scripts" {
+  metadata {
+    name      = "keycloak-custom-scripts"
+    namespace = kubernetes_namespace.keycloak_namespace.metadata[0].name
+  }
+
+  binary_data = {
+    "scripts.jar" = filebase64(data.archive_file.keycloak_scripts_jar.output_path)
+  }
+
+  depends_on = [data.archive_file.keycloak_scripts_jar]
+}
