c39462de9517d6f219d07cf6a6792291d043792b
Jack Chapman <jack.chapman@sky.com>
2023-07-12 21:54:03 +0100

    Create mapping between system_admins and Keycloak admin role.

---
+++ terraform/apps/keycloak-config/role_mapping.tf
@@ -0,0 +1,18 @@
+data "keycloak_group" "system_admins" {
+  realm_id = keycloak_realm.server_base_domain.id
+  name     = "system_admins"
+}
+
+data "keycloak_role" "admin" {
+  realm_id = keycloak_realm.server_base_domain.id
+  name     = "admin"
+}
+
+resource "keycloak_group_role" "system_admins_admin" {
+  realm_id = keycloak_realm.server_base_domain.id
+  group_id = data.keycloak_group.system_admins.id
+
+  role_ids = [
+    data.keycloak_role.admin
+  ]
+}
