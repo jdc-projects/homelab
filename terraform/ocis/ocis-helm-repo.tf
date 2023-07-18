f21612180debb6726a07614b4bcaaa6b93b7eba5
Jack Chapman <jack.chapman@sky.com>
2023-07-19 00:04:53 +0100

    Another unneeded file.

--- terraform/ocis/ocis-helm-repo.tf
+++
@@ -1,9 +0,0 @@
-# resource "null_resource" "ocis_helm_repo_clone" {
-#   triggers = {
-#     always_run = timestamp()
-#   }
-
-#   provisioner "local-exec" {
-#     command = "git clone --depth 1 -b v0.4.0 https://github.com/owncloud/ocis-charts.git"
-#   }
-# }
