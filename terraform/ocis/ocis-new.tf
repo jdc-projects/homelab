f6b7df1979ad0ead9475dc92a731c9448d91de38
Jack Chapman <jack.chapman@sky.com>
2023-07-18 23:13:10 +0100

    More organising.

--- terraform/ocis/ocis-new.tf
+++ terraform/ocis/ocis-new.tf
@@ -5,18 +5,15 @@ resource "kubernetes_secret" "ocis_config" {
   }
 
   data = {
-    STORAGE_SYSTEM_JWT_SECRET                  = random_password.storage_system_jwt_secret.result
-    WEB_OPTION_CONTEXTHELPERS_READ_MORE        = "true"
-    PROXY_ENABLE_BASIC_AUTH                    = "false"
-    PROXY_OIDC_REWRITE_WELLKNOWN               = "true"
-    PROXY_OIDC_SKIP_CLIENT_ID_CHECK            = "true"
-    PROXY_OIDC_INSECURE                        = "false"
-    PROXY_OIDC_USERINFO_CACHE_STORE            = "noop"
-    THUMBNAILS_TRANSFER_TOKEN                  = random_password.thumbnails_transfer_secret.result
-    GRAPH_LDAP_SERVER_UUID                     = "true"
-    GRAPH_LDAP_SERVER_USE_PASSWORD_MODIFY_EXOP = "false"
-    GRAPH_LDAP_REFINT_ENABLED                  = "false"
-    GRAPH_LDAP_GROUP_CREATE_BASE_DN            = "ou=groups,dc=idm,dc=${var.server_base_domain}"
+    STORAGE_SYSTEM_JWT_SECRET = random_password.storage_system_jwt_secret.result
+    PROXY_ENABLE_BASIC_AUTH   = "false"
+    # PROXY_OIDC_REWRITE_WELLKNOWN               = "true"
+    # PROXY_OIDC_SKIP_CLIENT_ID_CHECK            = "true"
+    PROXY_OIDC_INSECURE = "false"
+    # PROXY_OIDC_USERINFO_CACHE_STORE            = "noop"
+    THUMBNAILS_TRANSFER_TOKEN       = random_password.thumbnails_transfer_secret.result
+    GRAPH_LDAP_SERVER_UUID          = "true"
+    GRAPH_LDAP_GROUP_CREATE_BASE_DN = "ou=groups,dc=idm,dc=${var.server_base_domain}"
 
     NOTIFICATIONS_SMTP_HOST           = var.smtp_host
     NOTIFICATIONS_SMTP_PORT           = var.smtp_port
@@ -40,37 +37,36 @@ resource "kubernetes_secret" "ocis_config" {
     GATEWAY_STORAGE_USERS_MOUNT_ID   = random_uuid.storage_users_mount_id.result
     GRAPH_APPLICATION_ID             = random_uuid.graph_application_id.result
 
-    OCIS_LDAP_BIND_DN                        = "uid=${data.terraform_remote_state.openldap.outputs.admin_username},ou=people,dc=idm,dc=${var.server_base_domain}"
-    LDAP_BIND_PASSWORD                       = data.terraform_remote_state.openldap.outputs.admin_password
-    OCIS_LDAP_CACERT                         = ""
-    OCIS_LDAP_DISABLED_USERS_GROUP_DN        = "cn=app_disabled,ou=groups,dc=idm,dc=${var.server_base_domain}"
-    OCIS_LDAP_DISABLE_USER_MECHANISM         = "group"
-    OCIS_LDAP_GROUP_BASE_DN                  = "ou=groups,dc=idm,dc=${var.server_base_domain}"
-    OCIS_LDAP_GROUP_FILTER                   = ""
-    OCIS_LDAP_GROUP_OBJECTCLASS              = "groupOfNames"
-    OCIS_LDAP_GROUP_SCHEMA_DISPLAYNAME       = "cn"
-    OCIS_LDAP_GROUP_SCHEMA_GROUPNAME         = "cn"
-    OCIS_LDAP_GROUP_SCHEMA_ID                = "cn"
-    OCIS_LDAP_GROUP_SCHEMA_ID_IS_OCTETSTRING = "false"
-    OCIS_LDAP_GROUP_SCHEMA_MAIL              = "mail"
-    OCIS_LDAP_GROUP_SCHEMA_MEMBER            = "member"
-    OCIS_LDAP_GROUP_SCOPE                    = "sub"
-    OCIS_LDAP_INSECURE                       = "false"
-    OCIS_LDAP_SERVER_WRITE_ENABLED           = "false"
-    OCIS_LDAP_URI                            = "ldaps://idm.${var.server_base_domain}"
-    OCIS_LDAP_USER_BASE_DN                   = "ou=people,dc=idm,dc=${var.server_base_domain}"
-    OCIS_LDAP_USER_ENABLED_ATTRIBUTE         = "ownCloudUserEnabled"
-    OCIS_LDAP_USER_FILTER                    = ""
-    OCIS_LDAP_USER_OBJECTCLASS               = "inetOrgPerson"
-    OCIS_LDAP_USER_SCHEMA_DISPLAYNAME        = "displayname"
-    OCIS_LDAP_USER_SCHEMA_ID                 = "uid"
-    OCIS_LDAP_USER_SCHEMA_ID_IS_OCTETSTRING  = "false"
-    OCIS_LDAP_USER_SCHEMA_MAIL               = "mail"
-    OCIS_LDAP_USER_SCHEMA_USERNAME           = "uid"
-    # OCIS_LDAP_USER_SCHEMA_USER_TYPE = ""
-    OCIS_LDAP_USER_SCOPE                   = "sub"
-    USERS_LDAP_USER_SUBSTRING_FILTER_TYPE  = "any"
-    GROUPS_LDAP_USER_SUBSTRING_FILTER_TYPE = "any"
+    OCIS_LDAP_BIND_DN                          = "uid=${data.terraform_remote_state.openldap.outputs.admin_username},ou=people,dc=idm,dc=${var.server_base_domain}"
+    LDAP_BIND_PASSWORD                         = data.terraform_remote_state.openldap.outputs.admin_password
+    OCIS_LDAP_CACERT                           = ""
+    OCIS_LDAP_DISABLED_USERS_GROUP_DN          = "cn=app_disabled,ou=groups,dc=idm,dc=${var.server_base_domain}"
+    OCIS_LDAP_DISABLE_USER_MECHANISM           = "group"
+    OCIS_LDAP_GROUP_BASE_DN                    = "ou=groups,dc=idm,dc=${var.server_base_domain}"
+    OCIS_LDAP_GROUP_FILTER                     = ""
+    OCIS_LDAP_GROUP_OBJECTCLASS                = "groupOfNames"
+    OCIS_LDAP_GROUP_SCHEMA_DISPLAYNAME         = "cn"
+    OCIS_LDAP_GROUP_SCHEMA_GROUPNAME           = "cn"
+    OCIS_LDAP_GROUP_SCHEMA_ID                  = "cn"
+    OCIS_LDAP_GROUP_SCHEMA_ID_IS_OCTETSTRING   = "false"
+    OCIS_LDAP_GROUP_SCHEMA_MAIL                = "mail"
+    OCIS_LDAP_GROUP_SCHEMA_MEMBER              = "member"
+    OCIS_LDAP_GROUP_SCOPE                      = "sub"
+    OCIS_LDAP_INSECURE                         = "false"
+    OCIS_LDAP_SERVER_WRITE_ENABLED             = "false"
+    OCIS_LDAP_URI                              = "ldaps://idm.${var.server_base_domain}"
+    OCIS_LDAP_USER_BASE_DN                     = "ou=people,dc=idm,dc=${var.server_base_domain}"
+    OCIS_LDAP_USER_OBJECTCLASS                 = "inetOrgPerson"
+    OCIS_LDAP_USER_SCHEMA_DISPLAYNAME          = "displayname"
+    OCIS_LDAP_USER_SCHEMA_ID                   = "uid"
+    OCIS_LDAP_USER_SCHEMA_ID_IS_OCTETSTRING    = "false"
+    OCIS_LDAP_USER_SCHEMA_MAIL                 = "mail"
+    OCIS_LDAP_USER_SCHEMA_USERNAME             = "uid"
+    OCIS_LDAP_USER_SCOPE                       = "sub"
+    USERS_LDAP_USER_SUBSTRING_FILTER_TYPE      = "any"
+    GROUPS_LDAP_USER_SUBSTRING_FILTER_TYPE     = "any"
+    GRAPH_LDAP_SERVER_USE_PASSWORD_MODIFY_EXOP = "false"
+    GRAPH_LDAP_REFINT_ENABLED                  = "false"
 
     OCIS_OIDC_ISSUER                      = "https://idp.${var.server_base_domain}/realms/${var.server_base_domain}"
     WEB_OIDC_CLIENT_ID                    = keycloak_openid_client.ocis_web.client_id
