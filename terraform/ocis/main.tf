1d1d2993e8bfd6c6a6f0da3d221a74373f2d8786
Jack Chapman <jack.chapman@sky.com>
2023-07-19 00:03:31 +0100

    Remove unneeded provider.

--- terraform/ocis/main.tf
+++ terraform/ocis/main.tf
@@ -6,11 +6,6 @@ terraform {
   }
 
   required_providers {
-    helm = {
-      source  = "hashicorp/helm"
-      version = "2.10.1"
-    }
-
     kubernetes = {
       source  = "hashicorp/kubernetes"
       version = "2.21.1"
@@ -30,12 +25,6 @@ terraform {
       source  = "hashicorp/random"
       version = "3.5.1"
     }
-  }
-}
-
-provider "helm" {
-  kubernetes {
-    config_path = "../cluster.yml"
   }
 }
 
