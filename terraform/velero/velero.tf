1638806c2631d849885b33b8454c5f3928824ad9
Jack Chapman <jack.chapman@sky.com>
2023-07-19 11:01:31 +0100

    I keep being too optimistic on the timing...

--- terraform/velero/velero.tf
+++ terraform/velero/velero.tf
@@ -132,7 +132,7 @@ resource "helm_release" "velero" {
   }
   set {
     name  = "schedules.nightly.schedule"
-    value = "0 10 * * *"
+    value = "15 10 * * *"
   }
   set {
     name  = "schedules.nightly.useOwnerReferencesInBackup"
