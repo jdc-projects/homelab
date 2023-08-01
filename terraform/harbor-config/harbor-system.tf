fd56a9532d23993cfe963fe7459237ddacab1cd2
Jack Chapman <jack.chapman@sky.com>
2023-08-01 23:05:09 +0100

    Format.

--- terraform/harbor-config/harbor-system.tf
+++ terraform/harbor-config/harbor-system.tf
@@ -1,11 +1,11 @@
 resource "harbor_config_email" "main" {
-    email_host = var.smtp_host
-    email_port = var.smtp_port
-    email_username = var.smtp_username
-    email_password = var.smtp_password
-    email_from = "noreply@${var.server_base_domain}"
-    email_ssl = "true"
-    email_insecure = "false"
+  email_host     = var.smtp_host
+  email_port     = var.smtp_port
+  email_username = var.smtp_username
+  email_password = var.smtp_password
+  email_from     = "noreply@${var.server_base_domain}"
+  email_ssl      = "true"
+  email_insecure = "false"
 }
 
 resource "harbor_config_security" "main" {
