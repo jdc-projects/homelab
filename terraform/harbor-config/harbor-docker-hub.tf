b5a7ad928077ec0067188a27a6bc04674475bbb4
Jack Chapman <jack.chapman@sky.com>
2023-10-27 18:21:46 +0100

    Some optimisation and fixes.

--- terraform/harbor-config/harbor-docker-hub.tf
+++ terraform/harbor-config/harbor-docker-hub.tf
@@ -10,12 +10,12 @@ resource "harbor_project" "docker_hub" {
 }
 
 resource "harbor_retention_policy" "docker_hub" {
-    scope = harbor_project.docker_hub.id
-    schedule = "Daily"
+  scope    = harbor_project.docker_hub.id
+  schedule = "Daily"
 
-    rule {
-        n_days_since_last_pull = 30
-    }
+  rule {
+    n_days_since_last_pull = 30
+  }
 }
 
 resource "harbor_robot_account" "docker_hub_reader" {
@@ -35,38 +35,27 @@ resource "harbor_robot_account" "docker_hub_reader" {
   secret = random_password.docker_hub_reader_secret.result
 }
 
-resource "local_file" "k3s_registries_config" {
-  content  = <<-EOF
-    mirrors:
-      docker.io:
-        endpoint:
-          - "https://harbor.${var.server_base_domain}/${harbor_project.docker_hub.name}"
-    configs:
-      "harbor.${var.server_base_domain}/${harbor_project.docker_hub.name}":
-        auth:
-          username: ${harbor_robot_account.docker_hub_reader.full_name}
-          password: ${harbor_robot_account.docker_hub_reader.secret}
-  EOF
-  filename = "./registries.yaml"
-}
-
 resource "ssh_sensitive_resource" "k3s_registries_config_copy" {
   host        = var.truenas_ip_address
   user        = var.truenas_username
   private_key = var.truenas_ssh_private_key
 
   file {
-    source      = local_file.k3s_registries_config.filename
+    content     = <<-EOF
+      mirrors:
+        docker.io:
+          endpoint:
+            - "https://harbor.${var.server_base_domain}/${harbor_project.docker_hub.name}"
+      configs:
+        "harbor.${var.server_base_domain}/${harbor_project.docker_hub.name}":
+          auth:
+            username: ${harbor_robot_account.docker_hub_reader.full_name}
+            password: ${harbor_robot_account.docker_hub_reader.secret}
+    EOF
     destination = "/etc/rancher/k3s/registries.yaml"
   }
 
   commands = [
     "systemctl reload-or-restart k3s"
   ]
-
-  lifecycle {
-    replace_triggered_by = [
-      local_file.k3s_registries_config
-    ]
-  }
 }
