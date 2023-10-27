a00a830e90aac95629d6db5a1b98c75bea942e9e
Jack Chapman <jack.chapman@sky.com>
2023-10-27 18:08:54 +0100

    Add retention to the docker hub cache.

--- terraform/harbor-config/harbor-docker-hub.tf
+++ terraform/harbor-config/harbor-docker-hub.tf
@@ -9,6 +9,15 @@ resource "harbor_project" "docker_hub" {
   registry_id = harbor_registry.docker_hub.registry_id
 }
 
+resource "harbor_retention_policy" "docker_hub" {
+    scope = harbor_project.docker_hub.id
+    schedule = "Daily"
+
+    rule {
+        n_days_since_last_pull = 30
+    }
+}
+
 resource "harbor_robot_account" "docker_hub_reader" {
   name        = "docker-hub-reader"
   description = "Docker Hub proxy reader"
