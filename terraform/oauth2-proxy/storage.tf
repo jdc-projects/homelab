edc7020e47999e13214458cc4d50f9974ec74c0f
Jack Chapman <jack.chapman@sky.com>
2023-10-28 18:09:04 +0100

    Oauth2 proxy PVC name fix.

--- terraform/oauth2-proxy/storage.tf
+++ terraform/oauth2-proxy/storage.tf
@@ -1,6 +1,6 @@
 resource "kubernetes_persistent_volume_claim" "oauth2_proxy" {
   metadata {
-    name      = "oauth2_proxy"
+    name      = "oauth2-proxy"
     namespace = kubernetes_namespace.oauth2_proxy.metadata[0].name
   }
 
