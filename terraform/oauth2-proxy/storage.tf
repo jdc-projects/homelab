2490146c12c003609dbcb532cfc67c25001d1d7e
Jack Chapman <jack.chapman@sky.com>
2023-10-28 04:29:59 +0100

    Forgot a file.

---
+++ terraform/oauth2-proxy/storage.tf
@@ -0,0 +1,20 @@
+resource "kubernetes_persistent_volume_claim" "oauth2_proxy" {
+  metadata {
+    name      = "oauth2_proxy"
+    namespace = kubernetes_namespace.oauth2_proxy.metadata[0].name
+  }
+
+  spec {
+    access_modes = ["ReadWriteMany"]
+
+    resources {
+      requests = {
+        storage = "10Gi"
+      }
+    }
+  }
+
+  lifecycle {
+    prevent_destroy = false
+  }
+}
