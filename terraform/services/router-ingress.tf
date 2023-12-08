76b02460ee41e08b6b7c02d3bafd3c845e8170fd
Jack Chapman <jack.chapman@sky.com>
2023-12-08 22:43:44 +0000

    Router ingress using the wrong namespace.

--- terraform/services/router-ingress.tf
+++ terraform/services/router-ingress.tf
@@ -8,7 +8,7 @@ module "router_ingress" {
   source = "../modules/external-auth-ingress"
 
   server_base_domain = var.server_base_domain
-  namespace          = kubernetes_namespace.pve.metadata[0].name
+  namespace          = kubernetes_namespace.router.metadata[0].name
   external_name      = "192.168.1.1"
   external_scheme    = "https"
   external_port      = 444
