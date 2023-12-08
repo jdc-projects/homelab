46402176a3c31b0548ec4ada198267d4935440f2
Jack Chapman <jack.chapman@sky.com>
2023-12-08 10:56:53 +0000

    Missing file from last commit.

---
+++ terraform/velero/resource-policy.tf
@@ -0,0 +1,18 @@
+resource "kubernetes_config_map" "resource_policy" {
+  metadata {
+    name = "resource-policy"
+    namespace = kubernetes_namespace.velero.metadata[0].name
+  }
+
+  data = {
+    "resource-policy.yaml" = <<-EOF
+      version: v1
+      volumePolicies:
+      - conditions:
+          storageClass:
+          - truenas-nfs-csi-no-backup
+        action:
+          type: skip
+    EOF
+  }
+}
