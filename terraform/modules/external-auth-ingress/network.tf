236e309a50193733f11d0a2dbeb8c742293182c8
Jack Chapman <jack.chapman@sky.com>
2023-12-09 00:17:25 +0000

    Move oauth2-proxy headers ingress to modules.

--- terraform/modules/external-auth-ingress/network.tf
+++ terraform/modules/external-auth-ingress/network.tf
@@ -1,3 +1,33 @@
+resource "kubernetes_manifest" "forward_auth_headers_ingress" {
+  manifest = {
+    apiVersion = "traefik.io/v1alpha1"
+    kind       = "IngressRoute"
+
+    metadata = {
+      name      = "forward-auth-headers"
+      namespace = var.namespace
+    }
+
+    spec = {
+      entryPoints = ["websecure"]
+
+      routes = [{
+        kind  = "Rule"
+        match = "Host(`${var.url_subdomain}.${var.server_base_domain}`) && PathPrefix(`/oauth2/`)"
+        services = [{
+          name      = helm_release.oauth2_proxy.name
+          namespace = data.terraform_remote_state.oauth2_proxy.outputs.middleware_namespace
+          port      = "80"
+        }]
+        middlewares = [{
+          name      = kubernetes_manifest.oauth2_proxy_headers_middleware.manifest["metadata"].name
+          namespace = kubernetes_manifest.oauth2_proxy_headers_middleware.manifest["metadata"].namespace
+        }]
+      }]
+    }
+  }
+}
+
 resource "kubernetes_service" "forward_auth" {
   metadata {
     name      = "forward-auth"
