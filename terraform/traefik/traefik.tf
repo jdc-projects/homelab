33d607c908004f1a5e2f3a869efc869a8661cc26
Jack Chapman <jack.chapman@sky.com>
2023-12-18 12:05:30 +0000

    Traefik prometheus operator integration.

--- terraform/traefik/traefik.tf
+++ terraform/traefik/traefik.tf
@@ -77,6 +77,91 @@ resource "helm_release" "traefik" {
   }
 
   set {
+    name  = "metrics.prometheus"
+    value = "null"
+  }
+  set {
+    name  = "metrics.service.enabled"
+    value = "false"
+  }
+  set {
+    name  = "metrics.service.labels"
+    value = "{}"
+  }
+  set {
+    name  = "metrics.service.annotations"
+    value = "{}"
+  }
+  set {
+    name  = "metrics.disableAPICheck"
+    value = "false"
+  }
+  set_list {
+    name  = "metrics.serviceMonitor.metricRelabelings"
+    value = [
+      <<-EOF
+        sourceLabels: [__name__]
+        separator: ;
+        regex: ^fluentd_output_status_buffer_(oldest|newest)_.+
+        replacement: $1
+        action: drop
+      EOF
+      ,
+    ]
+  }
+  set_list {
+    name  = "metrics.serviceMonitor.relabelings"
+    value = [
+      <<-EOF
+        sourceLabels: [__meta_kubernetes_pod_node_name]
+        separator: ;
+        regex: ^(.*)$
+        targetLabel: nodename
+        replacement: $1
+        action: replace
+      EOF
+      ,
+    ]
+  }
+  set {
+    name  = "metrics.serviceMonitor.jobLabel"
+    value = "traefik"
+  }
+  set {
+    name  = "metrics.serviceMonitor.interval"
+    value = "30s"
+  }
+  set {
+    name  = "metrics.serviceMonitor.honorLabels"
+    value = "true"
+  }
+  set {
+    name  = "metrics.prometheusRule.additionalLabels"
+    value = "{}"
+  }
+  set {
+    name  = "metrics.prometheusRule.namespace"
+    value = kubernetes_namespace.traefik.metadata[0].name
+  }
+  set_list {
+    name  = "metrics.prometheusRule.rules"
+    value = [
+      <<-EOF
+        alert: TraefikDown
+        expr: up{job="traefik"} == 0
+        for: 5m
+        labels:
+          context: traefik
+          severity: warning
+        annotations:
+          summary: "Traefik Down"
+          description: "{{ $labels.pod }} on {{ $labels.nodename }} is down"
+      EOF
+      ,
+    ]
+  }
+
+  set {
     name  = "additionalArguments[0]"
     value = "--serverstransport.insecureskipverify=true"
   }
