bb446a56509c30777562fde05a4e9cf75239227e
Jack Chapman <jack.chapman@sky.com>
2023-12-19 15:06:53 +0000

    Openebs WIP.

---
+++ terraform/openebs/main.tf
@@ -0,0 +1,43 @@
+terraform {
+  backend "kubernetes" {
+    secret_suffix = "openebs"
+    config_path   = "../cluster.yml"
+    namespace     = "terraform-state"
+
+    labels = {
+      "velero.io/exclude-from-backup" = "true"
+    }
+  }
+
+  required_providers {
+    helm = {
+      source  = "hashicorp/helm"
+      version = "2.12.0"
+    }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.24.0"
+    }
+  }
+}
+
+provider "helm" {
+  kubernetes {
+    config_path = "../cluster.yml"
+  }
+}
+
+provider "kubernetes" {
+  config_path = "../cluster.yml"
+}
+
+resource "kubernetes_namespace" "openebs" {
+  metadata {
+    name = "openebs"
+
+    labels = {
+      "velero.io/exclude-from-backup" = "true"
+    }
+  }
+}
