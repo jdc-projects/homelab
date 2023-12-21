aea52228455c7d17b8500b49d3c7258ac3541298
Jack Chapman <jack.chapman@sky.com>
2023-12-21 01:37:03 +0000

    Cockpit namespace fix.

--- terraform/services/cockpit-ingress.tf
+++ terraform/services/cockpit-ingress.tf
@@ -8,7 +8,7 @@ module "cockpit_ingress" {
   source = "../modules/external-auth-ingress"
 
   server_base_domain = var.server_base_domain
-  namespace          = kubernetes_namespace.traefik_dashboard.metadata[0].name
+  namespace          = kubernetes_namespace.cockpit.metadata[0].name
   external_name      = "192.168.1.190"
   external_scheme    = "http"
   external_port      = 9090
