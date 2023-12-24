309ceb673b259189da3aa278fd580136aaecfd9e
Jack Chapman <jack.chapman@sky.com>
2023-12-24 18:24:44 +0000

    Backend config fix.

--- terraform/supabase/main.tf
+++ terraform/supabase/main.tf
@@ -2,7 +2,7 @@ terraform {
   backend "kubernetes" {
     secret_suffix = "supabase"
     config_path   = "../cluster.yml"
-    namespace     = "terraform-state"
+    namespace     = "tf-state"
 
     # ***** temporary
     labels = {
