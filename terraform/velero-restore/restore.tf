6a6f19a141bcaeb7ce2e678c7af1aaf106bbe90a
Jack Chapman <jack.chapman@sky.com>
2024-01-30 19:00:25 +0000

    Terraform format.

--- terraform/velero-restore/restore.tf
+++ terraform/velero-restore/restore.tf
@@ -9,7 +9,7 @@ resource "kubernetes_manifest" "velero_restore" {
     }
 
     spec = {
-      backupName              = "nightly-20231229022406"
+      backupName = "nightly-20231229022406"
       # scheduleName            = data.terraform_remote_state.velero.outputs.nightly_backup_name
       itemOperationTimeout    = "23h"
       includeClusterResources = true
