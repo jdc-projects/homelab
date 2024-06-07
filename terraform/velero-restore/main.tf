9654884a966175e1fa9ac29a9f0cd8bbf3a3f280
Jack Chapman <jack.chapman@sky.com>
2024-06-07 10:18:17 +0100

    Add velero-restore back in.

---
+++ terraform/velero-restore/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.24.0"
+    }
+  }
+}
+
+provider "kubernetes" {
+  config_path = "../cluster.yml"
+}
+
+data "terraform_remote_state" "velero" {
+  backend = "kubernetes"
+
+  config = {
+    secret_suffix = "velero"
+    config_path   = "../cluster.yml"
+    namespace     = "tf-state"
+  }
+}
