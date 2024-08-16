796531d3f7fe9a962249cd07befd44b4046be4a4
Jack Chapman <jack.chapman@sky.com>
2024-08-16 17:50:28 +0100

    Adding prometheus-blackbox-exporter.

---
+++ terraform/prometheus-operator/prometheus-blackbox-exporter.tf
@@ -0,0 +1,10 @@
+resource "helm_release" "prometheus_blackbox_exporter" {
+  name      = "prometheus-blackbox-exporter"
+  namespace = kubernetes_namespace.prometheus_operator.metadata[0].name
+
+  repository = "https://prometheus-community.github.io/helm-charts"
+  chart      = "prometheus-blackbox-exporter"
+  version    = "9.0.0"
+
+  timeout = 300
+}
