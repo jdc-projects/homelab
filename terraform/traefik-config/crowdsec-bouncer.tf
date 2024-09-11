c1a7159256a1e89f358adea3bc943e6388e21daf
Jack Chapman <jack.chapman@sky.com>
2024-09-11 17:11:38 +0100

    Format.

--- terraform/traefik-config/crowdsec-bouncer.tf
+++ terraform/traefik-config/crowdsec-bouncer.tf
@@ -19,10 +19,10 @@ resource "kubernetes_manifest" "crowdsec_bouncer_traefik_plugin_middleware" {
           # currently the helm chart doesn't support appsec - see https://github.com/maxlerebourg/crowdsec-bouncer-traefik-plugin/issues/150 - enable when it does
           CrowdsecAppsecEnabled = "false"
           # CrowdsecAppsecHost = "crowdsec:7422" # *****
-          CrowdsecLapiScheme         = "http"
-          CrowdsecLapiHost           = "${data.terraform_remote_state.crowdsec.outputs.crowdsec_helm_release_name}-service.${data.terraform_remote_state.crowdsec.outputs.crowdsec_namespace}:8080" # *****
-          CrowdsecLapiKey            = data.terraform_remote_state.crowdsec.outputs.traefik_api_key
-          ClientTrustedIPs           = [
+          CrowdsecLapiScheme = "http"
+          CrowdsecLapiHost   = "${data.terraform_remote_state.crowdsec.outputs.crowdsec_helm_release_name}-service.${data.terraform_remote_state.crowdsec.outputs.crowdsec_namespace}:8080" # *****
+          CrowdsecLapiKey    = data.terraform_remote_state.crowdsec.outputs.traefik_api_key
+          ClientTrustedIPs = [
             "192.168.1.0/24",
           ]
           ForwardedHeadersTrustedIPs = flatten(data.cloudflare_ip_ranges.cloudflare.cidr_blocks) # flatten is required to prevent an error for some reason...
