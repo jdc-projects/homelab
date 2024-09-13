5c03febabfed3645325a44aef586f2d0f22621f3
Jack Chapman <jack.chapman@sky.com>
2024-09-13 21:41:53 +0100

    Add master realm name to keycloak-config outputs.

--- terraform/keycloak-config/outputs.tf
+++ terraform/keycloak-config/outputs.tf
@@ -16,6 +16,10 @@ output "primary_realm_id" {
   value = keycloak_realm.primary.id
 }
 
+output "master_realm_id" {
+  value = data.keycloak_realm.master.id
+}
+
 output "keycloak_issuer_url" {
   value = "https://${data.terraform_remote_state.prometheus_operator.outputs.oauth_domain}/realms/${data.terraform_remote_state.prometheus_operator.outputs.oauth_realm_name}"
 }
