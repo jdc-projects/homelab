3648c0598f162af6e1e5eb40f50bc03e11a97458
Jack Chapman <jack.chapman@sky.com>
2024-09-14 00:23:10 +0100

    Remove oauth2-proxy and related modules, since they're no longer used.

--- terraform/modules/external-auth-ingress/main.tf
+++
@@ -1,9 +0,0 @@
-data "terraform_remote_state" "oauth2_proxy" {
-  backend = "kubernetes"
-
-  config = {
-    secret_suffix = "oauth2-proxy"
-    config_path   = "${path.module}/../../cluster.yml"
-    namespace     = "tf-state"
-  }
-}
