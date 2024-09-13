538e9c8878b98b340af5d6143ff651329f6e08b0
Jack Chapman <jack.chapman@sky.com>
2024-09-13 18:31:42 +0100

    Changes to whoami network to use new ingress module.

--- terraform/whoami/network.tf
+++ terraform/whoami/network.tf
@@ -1,59 +1,11 @@
-resource "kubernetes_service" "whoami" {
-  metadata {
-    name      = "whoami"
-    namespace = kubernetes_namespace.whoami.metadata[0].name
-  }
+module "whoami_ingress" {
+  source = "../modules/ingress"
 
-  spec {
-    selector = {
-      app = "whoami"
-    }
-
-    port {
-      port        = 80
-      target_port = 80
-    }
-  }
-}
-
-resource "kubernetes_manifest" "whoami_ingress" {
-  manifest = {
-    apiVersion = "traefik.io/v1alpha1"
-    kind       = "IngressRoute"
-
-    metadata = {
-      name      = "whoami"
-      namespace = kubernetes_namespace.whoami.metadata[0].name
-    }
-
-    spec = {
-      entryPoints = ["websecure"]
-
-      routes = [{
-        kind  = "Rule"
-        match = "Host(`whoami.${var.server_base_domain}`)"
-
-        services = [{
-          name      = kubernetes_service.whoami.metadata[0].name
-          namespace = kubernetes_namespace.whoami.metadata[0].name
-          port      = kubernetes_service.whoami.spec[0].port[0].port
-        }]
-
-        middlewares = [
-          {
-            name      = "cloudflare-real-ip"
-            namespace = "traefik"
-          },
-          {
-            name      = "geoblock"
-            namespace = "traefik"
-          },
-          {
-            name      = "crowdsec-bouncer"
-            namespace = "traefik"
-          },
-        ]
-      }]
-    }
+  name        = "whoami"
+  namespace   = kubernetes_namespace.whoami.metadata[0].name
+  domain      = "whoami.${var.server_base_domain}"
+  target_port = 80
+  selector    = {
+    app = "whoami"
   }
 }
