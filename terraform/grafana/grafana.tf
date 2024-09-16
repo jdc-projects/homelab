57ea87516aae9f460bc3656845e86ddf7c119c9d
Jack Chapman <jack.chapman@sky.com>
2024-09-16 22:08:07 +0100

    Some grafana fixing and tidying.

--- terraform/grafana/grafana.tf
+++ terraform/grafana/grafana.tf
@@ -38,21 +38,22 @@ resource "kubernetes_manifest" "grafana_deployment" {
         }
 
         "auth.generic_oauth" = {
-          enabled               = "true"
-          name                  = "Keycloak"
-          allow_sign_up         = "true"
-          client_id             = keycloak_openid_client.grafana.client_id
-          client_secret         = random_password.keycloak_client_secret.result
-          scopes                = "openid"
-          email_attribute_path  = "email"
-          login_attribute_path  = "preferred_username"
-          name_attribute_path   = "full_name"
-          groups_attribute_path = "groups"
-          auth_url              = data.terraform_remote_state.keycloak.outputs.keycloak_auth_url
-          token_url             = data.terraform_remote_state.keycloak.outputs.keycloak_token_url
-          api_url               = data.terraform_remote_state.keycloak.outputs.keycloak_api_url
-          role_attribute_path   = "contains(roles[*]\\, '${keycloak_role.grafana_admin.name}') && 'GrafanaAdmin'"
-          auto_login            = "true"
+          enabled                    = "true"
+          name                       = "Keycloak"
+          allow_sign_up              = "true"
+          client_id                  = keycloak_openid_client.grafana.client_id
+          client_secret              = random_password.keycloak_client_secret.result
+          scopes                     = "openid"
+          email_attribute_path       = "email"
+          login_attribute_path       = "preferred_username"
+          name_attribute_path        = "full_name"
+          groups_attribute_path      = "groups"
+          auth_url                   = data.terraform_remote_state.keycloak.outputs.keycloak_auth_url
+          token_url                  = data.terraform_remote_state.keycloak.outputs.keycloak_token_url
+          api_url                    = data.terraform_remote_state.keycloak.outputs.keycloak_api_url
+          role_attribute_path        = "contains(roles[*], '${keycloak_role.grafana_admin.name}') && 'GrafanaAdmin'"
+          auto_login                 = "true"
+          allow_assign_grafana_admin = "true"
         }
       }
     }
@@ -66,3 +67,17 @@ resource "kubernetes_manifest" "grafana_deployment" {
     }
   }
 }
+
+module "grafana_ingress" {
+  source = "../modules/ingress"
+
+  name      = "grafana"
+  namespace = kubernetes_namespace.grafana.metadata[0].name
+  domain    = local.grafana_domain
+
+  target_port = 3000
+
+  existing_service_name      = "${kubernetes_manifest.grafana_deployment.manifest.metadata.name}-service"
+  existing_service_namespace = kubernetes_manifest.grafana_deployment.manifest.metadata.namespace
+}
+
