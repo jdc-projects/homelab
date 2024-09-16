0d78f31c51a598285c229a0639ec131756b75489
Jack Chapman <jack.chapman@sky.com>
2024-09-16 22:18:49 +0100

    Remove Affine, since I don't plan on using it.

--- terraform/affine/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "affine_admin_email" {
-  value     = "${random_password.affine_admin_username.result}@${var.server_base_domain}"
-  sensitive = true
-}
-
-output "affine_admin_password" {
-  value     = random_password.affine_admin_password.result
-  sensitive = true
-}
