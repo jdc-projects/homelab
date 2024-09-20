fe764d6f1bdc23540380e3d0c6295b8c4c642922
Jack Chapman <jack.chapman@sky.com>
2024-09-20 18:46:03 +0100

    Keycloak temout values have to be in hours.

--- terraform/keycloak/realms.tf
+++ terraform/keycloak/realms.tf
@@ -14,12 +14,12 @@ resource "keycloak_realm" "primary" {
   login_with_email_allowed = false
   duplicate_emails_allowed = true
 
-  sso_session_idle_timeout = "7d"
-  sso_session_max_lifespan = "30d"
+  sso_session_idle_timeout = "120h" # 5 days
+  sso_session_max_lifespan = "720h" # 30 days
 
-  offline_session_idle_timeout         = "30d"
+  offline_session_idle_timeout         = "720h" # 30 days
   offline_session_max_lifespan_enabled = true
-  offline_session_max_lifespan         = "90d"
+  offline_session_max_lifespan         = "2160h" # 90 days
 
   depends_on = [
     null_resource.keycloak_liveness_check,
