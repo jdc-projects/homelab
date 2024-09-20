c0a74a50614e28a7e91f25ac39a3b244863b1884
Jack Chapman <jack.chapman@sky.com>
2024-09-20 18:32:25 +0100

    Format.

--- terraform/fission-setup/environments.tf
+++ terraform/fission-setup/environments.tf
@@ -2,25 +2,25 @@ resource "kubernetes_manifest" "fission_environment" {
   # https://fission.io/environments/
   for_each = tomap({
     nodejs = tomap({
-      version = 1
+      version       = 1
       runtime_image = "ghcr.io/fission/node-env:1.32.4"
       builder_image = null
-      poolsize = 3
+      poolsize      = 3
     })
     python = tomap({
-      version = 1
+      version       = 1
       runtime_image = "ghcr.io/fission/python-env:1.34.2"
       builder_image = null
-      poolsize = 3
+      poolsize      = 3
     })
   })
 
   manifest = {
     apiVersion = "fission.io/v1"
-    kind = "Environment"
+    kind       = "Environment"
 
     metadata = {
-      name = each.key
+      name      = each.key
       namespace = data.terraform_remote_state.fission.outputs.fission_environments_namespace
     }
 
