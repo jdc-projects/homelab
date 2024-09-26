7ab198fc16defabfd5891702457c8b552978dbe4
Jack Chapman <jack.chapman@sky.com>
2024-09-26 20:43:38 +0100

    Whoami file consolidation.

--- terraform/whoami/whoami.tf
+++ terraform/whoami/whoami.tf
@@ -41,3 +41,17 @@ resource "kubernetes_deployment" "whoami" {
     }
   }
 }
+
+module "whoami_ingress" {
+  source = "../modules/ingress"
+
+  name        = "whoami"
+  namespace   = kubernetes_namespace.whoami.metadata[0].name
+  domain      = "whoami.${var.server_base_domain}"
+  target_port = 80
+  selector = {
+    app = "whoami"
+  }
+
+  do_enable_keycloak_auth = true
+}
