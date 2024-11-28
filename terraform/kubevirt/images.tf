55314b64982816d05fcba921c0d76068111b4a00
Jack Chapman <jack.chapman@sky.com>
2024-11-28 14:41:24 +0000

    Setup download of images to PVCs for Kubevirt.

---
+++ terraform/kubevirt/images.tf
@@ -0,0 +1,48 @@
+resource "kubernetes_namespace" "kubevirt_images" {
+  metadata {
+    name = "kubevirt-images"
+  }
+}
+
+resource "kubernetes_persistent_volume_claim" "opnsense" {
+  for_each = tomap({
+    opnsense-24-7-nano-amd64 = tomap({
+      storage            = "4Gi"
+      storage_class_name = "openebs-zfs-localpv-bulk-no-backup"
+      endpoint           = "https://www.mirrorservice.org/sites/opnsense.org/releases/24.7/OPNsense-24.7-nano-amd64.img.bz2"
+    })
+  })
+
+  metadata {
+    name      = each.key
+    namespace = kubernetes_namespace.kubevirt_images.metadata[0].name
+
+    labels = {
+      app = "containerized-data-importer"
+    }
+
+    annotations = {
+      "cdi.kubevirt.io/storage.import.endpoint" = each.value.endpoint
+    }
+  }
+
+  spec {
+    access_modes       = ["ReadWriteOnce"]
+    storage_class_name = each.value.storage_class_name
+
+    resources {
+      requests = {
+        storage = each.value.storage
+      }
+    }
+  }
+
+  lifecycle {
+    prevent_destroy = false
+
+    ignore_changes = [
+      metadata[0].annotations,
+      spec[0].selector,
+    ]
+  }
+}
