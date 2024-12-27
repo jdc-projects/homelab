f7610d2595eb069ce12782907077de1d96d30198
Jack Chapman <jack.chapman@sky.com>
2024-12-27 01:13:00 +0000

    Silly mistake.

--- terraform/cluster-config-write.sh
+++ terraform/cluster-config-write.sh
@@ -1,6 +1,6 @@
 cp cluster.yml.template cluster.yml
 sed -i'' -e "s#{certificate-authority-data}#$CLUSTER_CA_CERT_DATA#g" ./cluster.yml
-sed -i'' -e "s#{server-ip}#$K3S_IP_ADDRESS#g" ./cluster.yml
+sed -i'' -e "s#{server-ip}#$SERVER_IP_ADDRESS#g" ./cluster.yml
 sed -i'' -e "s#{server-port}#$SERVER_KUBE_PORT#g" ./cluster.yml
 sed -i'' -e "s#{client-certificate-data}#$CLIENT_CERT_DATA#g" ./cluster.yml
 sed -i'' -e "s#{client-key-data}#$CLIENT_KEY_DATA#g" ./cluster.yml
