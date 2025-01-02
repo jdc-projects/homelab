c5da32752f2b26d1046cfd144fbc980c465a5cd7
Jack Chapman <jack.chapman@sky.com>
2025-01-02 19:10:19 +0000

    Fix for immediate vfio binding.

--- terraform/kubevirt/vfio-driver-bind.tf
+++ terraform/kubevirt/vfio-driver-bind.tf
@@ -238,7 +238,8 @@ resource "ssh_resource" "opnsense_nic_vfio_driver_binding" {
     "sudo mv ~/25-vfio-pci-bind.rules /lib/udev/rules.d/25-vfio-pci-bind.rules",
     "sudo mv ~/vfio-pci-bind.sh /lib/udev/vfio-pci-bind.sh",
     "sudo chmod +x /lib/udev/vfio-pci-bind.sh",
-    "sudo udevadm trigger",
+    "sudo /lib/udev/vfio-pci-bind.sh $(sudo lspci -d 8086:154d | awk '{print $1}' | sed '1!d')",
+    "sudo /lib/udev/vfio-pci-bind.sh $(sudo lspci -d 8086:154d | awk '{print $1}' | sed '2!d')",
   ]
 
   timeout = "1m"
