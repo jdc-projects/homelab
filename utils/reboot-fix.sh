9338fce9667a03a509d9ad3ca56e54a166dc9411
Jack Chapman <jack.chapman@sky.com>
2025-01-05 19:33:51 +0000

    Add github-org-runners to reboot script...

--- utils/reboot-fix.sh
+++ utils/reboot-fix.sh
@@ -1,3 +1,4 @@
 # temporary script to run after rebooting to deal with startup issues
 kubectl -n openebs delete pods --all &
 kubectl -n crowdsec delete pods --all &
+kubectl -n github-org-runners delete pods --all &
