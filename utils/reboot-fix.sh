6f35ef1c5b50feb3b92df8bfba363f569e4a7813
Jack Chapman <jack.chapman@sky.com>
2025-01-05 20:34:00 +0000

    Add traefik to reboot fix script.

--- utils/reboot-fix.sh
+++ utils/reboot-fix.sh
@@ -1,4 +1,5 @@
 # temporary script to run after rebooting to deal with startup issues
 kubectl -n openebs delete pods --all &
 kubectl -n crowdsec delete pods --all &
+kubectl -n traefik delete pods --all &
 terraform -chdir=../terraform/github-org-runners destroy -auto-approve -input=false && terraform -chdir=../terraform/github-org-runners apply -auto-approve -input=false
