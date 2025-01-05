43728d08186babd56efa8fe82d3ab09e4f545b1b
Jack Chapman <jack.chapman@sky.com>
2025-01-05 20:31:27 +0000

    Remove fork in reboot script for terraform.

--- utils/reboot-fix.sh
+++ utils/reboot-fix.sh
@@ -1,4 +1,4 @@
 # temporary script to run after rebooting to deal with startup issues
 kubectl -n openebs delete pods --all &
 kubectl -n crowdsec delete pods --all &
-terraform -chdir=../terraform/github-org-runners destroy -auto-approve -input=false && terraform -chdir=../terraform/github-org-runners apply -auto-approve -input=false &
+terraform -chdir=../terraform/github-org-runners destroy -auto-approve -input=false && terraform -chdir=../terraform/github-org-runners apply -auto-approve -input=false
