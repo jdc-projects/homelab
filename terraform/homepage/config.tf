85c78954cd28418c90155b0b6778d2137274fb78
Jack Chapman <jack.chapman@sky.com>
2025-01-30 17:58:29 +0000

    Homepage WIP commit.

---
+++ terraform/homepage/config.tf
@@ -0,0 +1,63 @@
+resource "kubernetes_config_map" "homepage_config_yamls" {
+  metadata {
+    name      = "homepage-config-yamls"
+    namespace = kubernetes_namespace.homepage.metadata[0].name
+  }
+
+  data = {
+    "settings.yaml" = <<-EOF
+      # https://gethomepage.dev/configs/settings/
+      title: ${var.server_base_domain} apps
+      description: Apps and services on ${var.server_base_domain}
+      # startUrl: PLACEHOLDER
+      # background:
+        # image: https://images.unsplash.com/photo-1502790671504-542ad42d5189?auto=format&fit=crop&w=2560&q=80 or /images/background.png
+        # blur: sm
+        # saturate: 50
+        # brightness: 50
+        # opacity: 50
+      # cardBlur: sm
+      # favicon: https://www.google.com/favicon.ico or ./favicon (path relative to /app/public)
+      theme: dark
+      color: slate
+      # layout: https://gethomepage.dev/configs/settings/#layout
+      headerStyle: underlined
+      # base: PLACEHOLDER
+      language: en-gb
+      # target: _blank
+      providers:
+      # openweathermap: openweathermapapikey
+      # finnhub: yourfinnhubapikeyhere
+      # longhorn:
+        # url: https://longhorn.example.com
+        # username: admin
+        # password: LonghornPassword
+      # quicklaunch:
+        # searchDescriptions: true
+        # hideInternetSearch: true
+        # showSearchSuggestions: true
+        # hideVisitURL: true
+        # provider: google
+      hideVersion: true
+      showStats: false
+      statusStyle: dot
+      # instanceName: homepage # *****
+      hideErrors: false
+    EOF
+    "bookmarks.yaml" = <<-EOF
+    EOF
+    "widgets.yaml" = <<-EOF
+    EOF
+    "services.yaml" = <<-EOF
+    EOF
+    "kubernetes.yaml" = <<-EOF
+      mode: disabled
+    EOF
+    "docker.yaml" = <<-EOF
+    EOF
+    "custom.css" = <<-EOF
+    EOF
+    "custom.js" = <<-EOF
+    EOF
+  }
+}
---
+++ terraform/homepage/homepage.tf
@@ -0,0 +1,110 @@
+resource "kubernetes_config_map" "homepage_env" {
+  metadata {
+    name      = "homepage"
+    namespace = kubernetes_namespace.homepage.metadata[0].name
+  }
+
+  data = {
+    NODE_ENV = "production"
+    LOG_TARGETS = "stdout"
+  }
+}
+
+resource "kubernetes_secret" "homepage_env" {
+  metadata {
+    name      = "homepage"
+    namespace = kubernetes_namespace.homepage.metadata[0].name
+  }
+
+  data = {
+  }
+}
+
+resource "kubernetes_deployment" "homepage" {
+  metadata {
+    name      = "homepage"
+    namespace = kubernetes_namespace.homepage.metadata[0].name
+  }
+
+  spec {
+    replicas = 1
+
+    selector {
+      match_labels = {
+        app = "homepage"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "homepage"
+        }
+      }
+
+      spec {
+        container {
+          image = "ghcr.io/gethomepage/homepage:v0.10.9"
+          name  = "homepage"
+
+          env_from {
+            config_map_ref {
+              name = kubernetes_config_map.homepage_env.metadata[0].name
+            }
+          }
+
+          env_from {
+            secret_ref {
+              name = kubernetes_secret.homepage_env.metadata[0].name
+            }
+          }
+
+          volume_mount {
+            name       = "config-yamls"
+            mount_path = "/app/config"
+          }
+
+          resources {
+            requests = {
+              cpu    = "100m"
+              memory = "256Mi"
+            }
+
+            limits = {
+              cpu    = "200m"
+              memory = "512Mi"
+            }
+          }
+        }
+
+        volume {
+          name = "config-yamls"
+
+          config_map {
+            name = kubernetes_config_map.homepage_config_yamls.metadata[0].name
+          }
+        }
+      }
+    }
+  }
+
+  lifecycle {
+    replace_triggered_by = [
+      kubernetes_config_map.homepage_config_yamls,
+    ]
+  }
+}
+
+module "homepage_ingress" {
+  source = "../modules/ingress"
+
+  name        = "homepage"
+  namespace   = kubernetes_namespace.homepage.metadata[0].name
+  domain      = "apps.${var.server_base_domain}"
+  target_port = 3000
+  selector = {
+    app = "homepage"
+  }
+
+  do_enable_keycloak_auth = true
+}
