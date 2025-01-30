ed9dbdd8cb06aeed0add6ec62a5d718e32ac6db0
Jack Chapman <jack.chapman@sky.com>
2025-01-30 10:50:31 +0000

    Use helm chart for the clickhouse operator.

--- terraform/clickhouse-operator/clickhouse-operator.tf
+++ terraform/clickhouse-operator/clickhouse-operator.tf
@@ -1,21 +1,38 @@
-resource "null_resource" "clickhouse_operator" {
+# https://github.com/Altinity/clickhouse-operator/tree/master/deploy/helm/clickhouse-operator
+
+resource "null_resource" "clickhouse_operator_crds" {
   triggers = {
-    always_run = timestamp()
-    namespace  = "clickhouse-operator"
-    version    = "release-0.24.3"
+    version    = "0.24.3"
   }
 
   provisioner "local-exec" {
     when    = create
     command = <<-EOF
-      curl -s https://raw.githubusercontent.com/Altinity/clickhouse-operator/${self.triggers.version}/deploy/operator-web-installer/clickhouse-operator-install.sh | OPERATOR_NAMESPACE=${self.triggers.namespace} METRICS_EXPORTER_NAMESPACE=${self.triggers.namespace} bash
+      kubectl apply -f https://raw.githubusercontent.com/Altinity/clickhouse-operator/release-${self.triggers.version}/deploy/helm/clickhouse-operator/crds/CustomResourceDefinition-clickhouseinstallations.clickhouse.altinity.com.yaml
+      kubectl apply -f https://raw.githubusercontent.com/Altinity/clickhouse-operator/release-${self.triggers.version}/deploy/helm/clickhouse-operator/crds/CustomResourceDefinition-clickhouseinstallationtemplates.clickhouse.altinity.com.yaml
+      kubectl apply -f https://raw.githubusercontent.com/Altinity/clickhouse-operator/release-${self.triggers.version}/deploy/helm/clickhouse-operator/crds/CustomResourceDefinition-clickhouseoperatorconfigurations.clickhouse.altinity.com.yaml
+      kubectl apply -f https://raw.githubusercontent.com/Altinity/clickhouse-operator/release-${self.triggers.version}/deploy/helm/clickhouse-operator/crds/CustomResourceDefinition-clickhousekeeperinstallations.clickhouse-keeper.altinity.com.yaml
     EOF
   }
 
   provisioner "local-exec" {
     when    = destroy
     command = <<-EOF
-      curl -s https://raw.githubusercontent.com/Altinity/clickhouse-operator/${self.triggers.version}/deploy/operator-web-installer/clickhouse-operator-delete.sh | OPERATOR_NAMESPACE=${self.triggers.namespace} METRICS_EXPORTER_NAMESPACE=${self.triggers.namespace} bash
+      kubectl delete -f https://raw.githubusercontent.com/Altinity/clickhouse-operator/release-${self.triggers.version}/deploy/helm/clickhouse-operator/crds/CustomResourceDefinition-clickhouseinstallations.clickhouse.altinity.com.yaml
+      kubectl delete -f https://raw.githubusercontent.com/Altinity/clickhouse-operator/release-${self.triggers.version}/deploy/helm/clickhouse-operator/crds/CustomResourceDefinition-clickhouseinstallationtemplates.clickhouse.altinity.com.yaml
+      kubectl delete -f https://raw.githubusercontent.com/Altinity/clickhouse-operator/release-${self.triggers.version}/deploy/helm/clickhouse-operator/crds/CustomResourceDefinition-clickhouseoperatorconfigurations.clickhouse.altinity.com.yaml
+      kubectl delete -f https://raw.githubusercontent.com/Altinity/clickhouse-operator/release-${self.triggers.version}/deploy/helm/clickhouse-operator/crds/CustomResourceDefinition-clickhousekeeperinstallations.clickhouse-keeper.altinity.com.yaml
     EOF
   }
+}
+
+resource "helm_release" "clickhouse_operator" {
+  name      = "clickhouse-operator"
+  namespace = kubernetes_namespace.clickhouse_operator.metadata[0].name
+
+  repository = "https://docs.altinity.com/clickhouse-operator"
+  chart      = "altinity-clickhouse-operator"
+  version    = null_resource.clickhouse_operator_crds.triggers.version
+
+  timeout = 300
 }
--- terraform/clickhouse-operator/main.tf
+++ terraform/clickhouse-operator/main.tf
@@ -4,4 +4,32 @@ terraform {
     config_path   = "../cluster.yml"
     namespace     = "tf-state"
   }
+
+  required_providers {
+    helm = {
+      source  = "hashicorp/helm"
+      version = "2.13.2"
+    }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.30.0"
+    }
+  }
+}
+
+provider "helm" {
+  kubernetes {
+    config_path = "../cluster.yml"
+  }
+}
+
+provider "kubernetes" {
+  config_path = "../cluster.yml"
+}
+
+resource "kubernetes_namespace" "clickhouse_operator" {
+  metadata {
+    name = "clickhouse-operator"
+  }
 }
