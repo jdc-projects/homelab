8063f32bbcb8481d81559ac16d5a4d261fcd2b1b
Jack Chapman <jack.chapman@sky.com>
2025-03-26 16:08:26 +0000

    Prevent minio data deletion.

--- terraform/minio/storage.tf
+++ terraform/minio/storage.tf
@@ -16,7 +16,7 @@ resource "kubernetes_persistent_volume_claim" "minio" {
   }
 
   lifecycle {
-    prevent_destroy = false # ***** true
+    prevent_destroy = true
 
     ignore_changes = [spec[0].selector]
   }
