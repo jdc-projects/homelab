793c13de9e0f7c89fb71c97658bb9d7c8e42632d
Jack Chapman <jack.chapman@sky.com>
2025-11-26 13:21:02 +0000

    Github runner updates.

--- terraform/github-org-runners/main.tf
+++ terraform/github-org-runners/main.tf
@@ -43,6 +43,6 @@ resource "kubernetes_namespace" "github_org_runners" {
 }
 
 locals {
-  arc_version    = "0.12.1"
-  runner_version = "2.328.0"
+  arc_version    = "0.13.0"
+  runner_version = "2.330.0"
 }
