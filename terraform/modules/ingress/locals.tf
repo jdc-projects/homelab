913bb4d0097cd1465aa94770d7dd42f5bfdbe18c
Jack Chapman <jack.chapman@sky.com>
2026-01-09 18:20:54 +0000

    Remove marking comment in ingress module.

--- terraform/modules/ingress/locals.tf
+++ terraform/modules/ingress/locals.tf
@@ -17,7 +17,7 @@ locals {
       }] : var.do_enable_crowdsec_bouncer && !var.do_enable_crowdsec_bouncer_appsec ? [{
       name      = "crowdsec-bouncer-without-appsec"
       namespace = data.terraform_remote_state.traefik.outputs.traefik_namespace
-    }] : [], # *****
+    }] : [],
     var.do_enable_api_key_auth ? [{
       name      = one(kubernetes_manifest.api_key_auth_plugin_middleware[*].manifest.metadata.name)
       namespace = var.namespace
