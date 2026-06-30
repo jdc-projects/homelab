53f593f913abc8aa03c741a2604810e9ad37181e
Jack Chapman <jack.chapman@sky.com>
2026-06-30 17:56:47 +0100

    Upgrade cert-manager chart v1.19.1 -> v1.20.3 and bump providers

--- terraform/cert-manager/cert-manager.tf
+++ terraform/cert-manager/cert-manager.tf
@@ -3,7 +3,7 @@ resource "helm_release" "cert_manager" {
 
   repository = "https://charts.jetstack.io"
   chart      = "cert-manager"
-  version    = "v1.19.1"
+  version    = "v1.20.3"
 
   namespace = kubernetes_namespace.cert_manager.metadata[0].name
 
