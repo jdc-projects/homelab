0968436d90ca2f35551d10e7270f6ef10905c261
Jack Chapman <jack.chapman@sky.com>
2026-06-30 18:10:34 +0100

    Upgrade vaultwarden server 1.34.3 -> 1.36.0, ldap 2.2.0 -> 2.2.1, postgres 16.11 -> 16.14, bump providers

--- terraform/vaultwarden/main.tf
+++ terraform/vaultwarden/main.tf
@@ -8,12 +8,12 @@ terraform {
   required_providers {
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "2.38.0"
+      version = "3.2.0"
     }
 
     keycloak = {
       source  = "keycloak/keycloak"
-      version = "5.5.0"
+      version = "5.8.0"
     }
   }
 }
