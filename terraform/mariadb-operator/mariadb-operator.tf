16582933ed0ac236374c78010312db8387d8a1c7
Jack Chapman <jack.chapman@sky.com>
2026-07-07 17:32:51 +0100

    Add mariadb-operator and hibernation pipeline integration.

---
+++ terraform/mariadb-operator/mariadb-operator.tf
@@ -0,0 +1,28 @@
+locals {
+  chart_repository = "oci://ghcr.io/mariadb-operator/charts"
+  chart_version    = "26.6.0"
+}
+
+resource "helm_release" "mariadb_operator_crds" {
+  name      = "mariadb-operator-crds"
+  namespace = kubernetes_namespace.mariadb_operator.metadata[0].name
+
+  repository = local.chart_repository
+  chart      = "mariadb-operator-crds"
+  version    = local.chart_version
+
+  timeout = 300
+}
+
+resource "helm_release" "mariadb_operator" {
+  name      = "mariadb-operator"
+  namespace = kubernetes_namespace.mariadb_operator.metadata[0].name
+
+  repository = local.chart_repository
+  chart      = "mariadb-operator"
+  version    = local.chart_version
+
+  timeout = 300
+
+  depends_on = [helm_release.mariadb_operator_crds]
+}
