dc4d007eb27579205b60473b39fb7090820b38f4
Jack Chapman <jack.chapman@sky.com>
2026-08-05 18:49:41 +0100

    prometheus: add Alertmanager datasource for Grafana alerting
    External Alertmanagers must be registered as an alertmanager-type Grafana
    datasource for the Alerting UI (Active notifications / alert groups, silences)
    to read from them. Points at kube-prometheus-stack-alertmanager. Fixes the 404
    on Grafana's Active notifications page, which had no Alertmanager source to
    render against (alerts fire in Prometheus/Alertmanager, not Grafana's empty
    internal AM).

--- terraform/prometheus/grafana-datasource.tf
+++ terraform/prometheus/grafana-datasource.tf
@@ -35,3 +35,45 @@ resource "kubernetes_manifest" "prometheus_grafana_datasource" {
 
   depends_on = [helm_release.kube_prometheus_stack]
 }
+
+# Grafana datasource for the kube-prometheus-stack Alertmanager. External
+# Alertmanagers must be wired into Grafana as a datasource; the legacy
+# [unified_alerting] alertmanager_url key no longer registers one. This lets
+# the Alerting UI (Active notifications / alert groups, silences) read from the
+# Prometheus Alertmanager where alerts actually fire. Same cross-namespace
+# import pattern as the Prometheus datasource above.
+
+resource "kubernetes_manifest" "alertmanager_grafana_datasource" {
+  manifest = {
+    apiVersion = "grafana.integreatly.org/v1beta1"
+    kind       = "GrafanaDatasource"
+
+    metadata = {
+      name      = "alertmanager"
+      namespace = kubernetes_namespace.prometheus.metadata[0].name
+    }
+
+    spec = {
+      allowCrossNamespaceImport = "true"
+
+      instanceSelector = {
+        matchLabels = data.terraform_remote_state.grafana.outputs.grafana_deployment_labels
+      }
+
+      datasource = {
+        name = "Alertmanager"
+        type = "alertmanager"
+
+        access = "proxy"
+
+        url = "http://${helm_release.kube_prometheus_stack.name}-alertmanager.${kubernetes_namespace.prometheus.metadata[0].name}.svc.cluster.local:9093"
+
+        jsonData = {
+          implementation = "prometheus"
+        }
+      }
+    }
+  }
+
+  depends_on = [helm_release.kube_prometheus_stack]
+}
